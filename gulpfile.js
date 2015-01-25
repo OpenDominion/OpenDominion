35001a0c3bcb45cb9def5c4d05246998522da75e
WaveHack <email@wavehack.net>
2015-01-25 16:42:57 +0100

    Adding Bootstrap through Bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,67 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	minify = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	phpunit = require('gulp-phpunit');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var paths = {
+	'src': {
+		'less': './resources/assets/less/',
+		'js': './resources/assets/js/',
+		'vendor': './resources/assets/vendor/'
+	},
+	'dst': {
+		'css': './public/assets/css/',
+		'js': './public/assets/js/'
+	}
+};
 
-elixir(function(mix) {
-    mix.less('app.less');
+// CSS
+gulp.task('css', function () {
+	return gulp.src(paths.src.less + 'app.less')
+		.pipe(less())
+		.pipe(gulp.dest(paths.dst.css))
+		.pipe(minify({keepSpecialComments: 0}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(paths.dst.css));
 });
+
+// JS
+gulp.task('js', function () {
+	return gulp.src([
+		paths.src.vendor + 'jquery/dist/jquery.js',
+		paths.src.vendor + 'bootstrap/dist/js/bootstrap.js',
+		paths.src.js + 'app.js'
+	])
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.dst.js));
+});
+
+// PHP Unit
+//gulp.task('phpunit', function () {
+//	var options = {debug: false, notify: true};
+//	return gulp.src('./tests/*.php')
+//		.pipe(phpunit('./vendor/bin/phpunit', options))
+//
+//		.on('error', notify.onError({
+//			title: 'PHPUnit Failed',
+//			message: 'One or more tests failed.'
+//		}))
+//
+//		.pipe(notify({
+//			title: 'PHPUnit Passed',
+//			message: 'All tests passed!'
+//		}));
+//});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.src.less + '/*.less', ['css']);
+	gulp.watch(paths.src.js + '/*.js', ['js']);
+	//gulp.watch('./tests/*.php', ['phpunit']);
+});
+
+gulp.task('default', ['css', 'js', /*'phpunit',*/ 'watch']);
