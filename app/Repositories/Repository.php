5f674bf99ed96de30a379f334801ffa9f68917c9
WaveHack <email@wavehack.net>
2015-03-05 08:23:51 +0100

    Updated repositories

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -1,5 +1,23 @@
 <?php namespace OpenDominion\Repositories;
 
+use Illuminate\Database\Eloquent\Model;
+
 abstract class Repository
 {
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,6 +1,16 @@
 <?php namespace OpenDominion\Repositories;
 
+use OpenDominion\Models\User;
+
 class UserRepository extends Repository
 {
+    public function __construct(User $model)
+    {
+        parent::__construct($model);
+    }
 
+    public function findByEmail($email)
+    {
+        return $this->model->where('email', $email)->first();
+    }
 }
