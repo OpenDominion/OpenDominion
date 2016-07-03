d5370b3ae9782d35e04ed45748775000f4146143
WaveHack <email@wavehack.net>
2016-07-03 23:36:15 +0200

    Add $guarded prop on RacePerk model

--- app/Models/RacePerk.php
+++ app/Models/RacePerk.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RacePerk extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     public function race()
     {
         return $this->belongsTo(Race::class);
