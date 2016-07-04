4c513b450b66bea7da88eb8d5dbefe0c07d42b86
WaveHack <email@wavehack.net>
2016-07-05 00:58:17 +0200

    Remove redundant docblock comment of redundancy

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -8,11 +8,6 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends BaseTestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
     {
         $this->visit('/')
