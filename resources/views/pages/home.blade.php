b9b85c33a8590edd06d2d1f973034ae736a40b95
WaveHack <email@wavehack.net>
2016-07-24 13:25:31 +0200

    Update home view content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,8 +6,8 @@
     <div class="row">
         <div class="col-lg-12">
             <p>OpenDominion is a free online text-based strategy game based on Dominion, a defunct online text-based strategy game from Kamikaze Games which ran from about 2000 to 2012.</p>
-            <p>To start playing, <a href="{{ route('auth.register') }}">register</a> an account and sign up for a round after registration.</p>
-            <p>OpenDominion is open source software and can be found on <a href="https://github.com/WaveHack/OpenDominion" target="_blank">GitHub</a>.</p>
+            <p>To start playing, <a href="{{ route('auth.register') }}">register</a> an account and sign up for a round after registration. If you already have an account, <a href="{{ route('auth.login') }}">login</a> instead.</p>
+            <p>OpenDominion is open source software and can be found on <a href="https://github.com/WaveHack/OpenDominion" target="_blank">GitHub <i class="fa fa-external-link"></i></a>.</p>
         </div>
     </div>
 @endsection
