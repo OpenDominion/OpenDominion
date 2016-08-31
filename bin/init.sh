b3f16bafa463e649c9fd5aa2496da7b16f86948f
WaveHack <email@wavehack.net>
2016-08-31 13:45:08 +0200

    Refactor init script a bit

--- bin/init.sh
+++ bin/init.sh
@@ -7,16 +7,18 @@ if [[ ! $# -eq 1 ||  ! $1 =~ ^(local|production|testing)$ ]]; then
     exit 1;
 fi
 
+env=$1
+
 # Composer
 composer self-update
 
 if [[ ! -d vendor ]]; then
-    if [[ $1 == production ]]; then
+    if [[ ${env} == production ]]; then
         composer install --no-interaction --prefer-dist --no-dev
     else
         composer install --no-interaction --prefer-source
 
-        if [[ $1 == local ]] && grep -q laravel-ide-helper composer.json; then
+        if [[ ${env} == local ]] && grep -q laravel-ide-helper composer.json; then
             IDE_HELPERS=1
         fi
     fi
@@ -24,20 +26,20 @@ fi
 
 # Dotenv
 if [[ ! -f .env ]]; then
-    cp ".env.template.$1" .env
+    cp ".env.template.$env" .env
 
     # Generate app key
     php bin/artisan key:generate
 fi
 
 # Setup/seed database + IDE files on local
-if [[ $1 == local ]] && [[ ! -f app/storage/databases/local.sqlite ]]; then
+if [[ ${env} == local ]] && [[ ! -f app/storage/databases/local.sqlite ]]; then
     touch app/storage/databases/local.sqlite
     php bin/artisan migrate --seed
 fi
 
 # Local IDE Helpers
-if [[ $IDE_HELPERS ]]; then
+if [[ ${IDE_HELPERS} ]]; then
     php bin/artisan clear-compiled
     php bin/artisan ide-helper:generate
     php bin/artisan ide-helper:models -N
@@ -46,7 +48,7 @@ if [[ $IDE_HELPERS ]]; then
 fi
 
 # Frontend stuff, not needed during testing
-if [[ ! $1 == testing ]]; then
+if [[ ! ${env} == testing ]]; then
 
     # Npm packages
     [[ -d node_modules ]] || npm install
@@ -56,7 +58,7 @@ if [[ ! $1 == testing ]]; then
     [[ -f bower.json && ! -d bower_components ]] && bower install
 
     # Gulp
-    if [[ $1 == production ]]; then
+    if [[ ${env} == production ]]; then
         gulp --production
     else
         gulp
@@ -65,7 +67,7 @@ if [[ ! $1 == testing ]]; then
 fi
 
 # Show message on production
-if [[ $1 == production ]]; then
+if [[ ${env} == production ]]; then
     echo "Don't forget to setup your .env file and run 'php bin/artisan migrate --seed'"
 fi
 
