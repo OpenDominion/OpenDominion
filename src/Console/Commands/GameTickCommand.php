01834a865ec7cca60c70dd393f6502d5f91485f3
WaveHack <email@wavehack.net>
2016-09-10 23:12:37 +0200

    Add empty game tick command

---
+++ src/Console/Commands/GameTickCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OpenDominion\Console\Commands;
+
+use Illuminate\Console\Command;
+use Log;
+
+class GameTickCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'game:tick';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Ticks the game';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info('Tick');
+
+        // todo: implement me
+    }
+}
--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        \OpenDominion\Console\Commands\GameTickCommand::class,
     ];
 
     /**
@@ -23,6 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('game:tick')
+            ->hourly();
     }
 
     /**
