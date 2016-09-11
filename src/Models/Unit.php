ca00790747979eba1a1f8b5179d69cdf5dc24438
WaveHack <email@wavehack.net>
2016-09-11 20:00:57 +0200

    Implement unit networth

--- src/Models/Unit.php
+++ src/Models/Unit.php
@@ -13,4 +13,17 @@ class Unit extends AbstractModel
     {
         return $this->hasOne(Race::class);
     }
+
+    public function getNetworth()
+    {
+        if (in_array($this->slot, [1, 2])) {
+            return 5;
+        }
+
+        return (
+            (1.8 * min(6, max($this->power_offense, $this->power_defense)))
+            + (0.45 * min(6, min($this->power_offense, $this->power_defense)))
+            + (0.2 * (max(($this->power_offense - 6), 0) + max(($this->power_defense - 6), 0)))
+        );
+    }
 }
