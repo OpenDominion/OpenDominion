355ffebfed08ef16e460db11c3464b0eca70461c
WaveHack <email@wavehack.net>
2016-09-11 18:05:25 +0200

    Implement NewDominionGetsPlacedInARealmBasedOnRaceAlignment test

--- tests/Feature/RealmTest.php
+++ tests/Feature/RealmTest.php
@@ -2,7 +2,9 @@
 
 namespace OpenDominion\Tests\Feature;
 
+use CoreDataSeeder;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use OpenDominion\Models\Race;
 use OpenDominion\Tests\BaseTestCase;
 
 class RealmTest extends BaseTestCase
@@ -11,7 +13,36 @@ class RealmTest extends BaseTestCase
 
     public function testNewDominionGetsPlacedInARealmBasedOnRaceAlignment()
     {
-        $this->markTestIncomplete();
+        $this->seed(CoreDataSeeder::class);
+
+        $round = $this->createRound();
+
+        $this
+            ->dontSeeInDatabase('realms', ['alignment' => 'good'])
+            ->dontSeeInDatabase('realms', ['alignment' => 'evil']);
+
+        $goodRace = Race::where('alignment', 'good')->firstOrFail();
+        $evilRace = Race::where('alignment', 'evil')->firstOrFail();
+
+        $userWithGoodDominion = $this->createUser();
+        $goodDominion = $this->createDominion($userWithGoodDominion, $round, $goodRace);
+
+        $anotherUserWithGoodDominion = $this->createUser();
+        $anotherGoodDominion = $this->createDominion($anotherUserWithGoodDominion, $round, $goodRace);
+
+        $userWithEvilDominion = $this->createUser();
+        $evilDominion = $this->createDominion($userWithEvilDominion, $round, $evilRace);
+
+        $anotherUserWithEvilDominion = $this->createUser();
+        $anotherEvilDominion = $this->createDominion($anotherUserWithEvilDominion, $round, $evilRace);
+
+        $this
+            ->seeInDatabase('realms', ['alignment' => 'good'])
+            ->seeInDatabase('realms', ['alignment' => 'evil'])
+            ->seeInDatabase('dominions', ['id' => $goodDominion->id, 'realm_id' => 1])
+            ->seeInDatabase('dominions', ['id' => $anotherGoodDominion->id, 'realm_id' => 1])
+            ->seeInDatabase('dominions', ['id' => $evilDominion->id, 'realm_id' => 2])
+            ->seeInDatabase('dominions', ['id' => $anotherEvilDominion->id, 'realm_id' => 2]);
     }
 
     public function testRealmsCantContainMoreThan15Dominions()
