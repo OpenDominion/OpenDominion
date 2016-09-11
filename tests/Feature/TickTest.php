757a883dbee2b11298079d967ea254b2e1acbcac
WaveHack <email@wavehack.net>
2016-09-11 19:51:39 +0200

    Add QueuesTick test

--- tests/Feature/TickTest.php
+++ tests/Feature/TickTest.php
@@ -4,6 +4,7 @@ namespace OpenDominion\Tests\Feature;
 
 use Artisan;
 use CoreDataSeeder;
+use DB;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use OpenDominion\Tests\BaseTestCase;
 
@@ -23,10 +24,69 @@ class TickTest extends BaseTestCase
 
         // Test +6 morale below 70
         Artisan::call('game:tick');
-        $this->seeInDatabase('dominions', [ 'id' => $dominion->id, 'morale' => 70]);
+        $this->seeInDatabase('dominions', ['id' => $dominion->id, 'morale' => 70]);
 
         // Test +3 morale above 70
         Artisan::call('game:tick');
-        $this->seeInDatabase('dominions', [ 'id' => $dominion->id, 'morale' => 73]);
+        $this->seeInDatabase('dominions', ['id' => $dominion->id, 'morale' => 73]);
+    }
+
+    public function testQueuesTick()
+    {
+        $this->seed(CoreDataSeeder::class);
+        $user = $this->createUser();
+        $round = $this->createRound();
+        $dominion = $this->createDominion($user, $round);
+
+        $dominion->land_plain = 0;
+        $dominion->building_home = 0;
+        $dominion->save();
+
+        DB::table('queue_exploration')->insert([
+            'dominion_id' => $dominion->id,
+            'land_type' => 'plain',
+            'amount' => 10,
+            'hours' => 2,
+        ]);
+
+        DB::table('queue_construction')->insert([
+            'dominion_id' => $dominion->id,
+            'building' => 'home',
+            'amount' => 10,
+            'hours' => 2,
+        ]);
+
+        // Test queue hours 2 -> 1
+        Artisan::call('game:tick');
+        $this
+            ->seeInDatabase('dominions', [
+                'id' => $dominion->id,
+                'land_plain' => 0,
+                'building_home' => 0,
+            ])
+            ->seeInDatabase('queue_exploration', ['dominion_id' => $dominion->id, 'land_type' => 'plain', 'hours' => 1])
+            ->seeInDatabase('queue_construction', ['dominion_id' => $dominion->id, 'building' => 'home', 'hours' => 1]);
+
+        // Test queue hours 1 -> 0
+        Artisan::call('game:tick');
+        $this
+            ->seeInDatabase('dominions', [
+                'id' => $dominion->id,
+                'land_plain' => 0,
+                'building_home' => 0,
+            ])
+            ->seeInDatabase('queue_exploration', ['dominion_id' => $dominion->id, 'land_type' => 'plain', 'hours' => 0])
+            ->seeInDatabase('queue_construction', ['dominion_id' => $dominion->id, 'building' => 'home', 'hours' => 0]);
+
+        // Test queues get processed on hour 0
+        Artisan::call('game:tick');
+        $this
+            ->seeInDatabase('dominions', [
+                'id' => $dominion->id,
+                'land_plain' => 10,
+                'building_home' => 10,
+            ])
+            ->dontSeeInDatabase('queue_exploration', ['dominion_id' => $dominion->id, 'land_type' => 'plain'])
+            ->dontSeeInDatabase('queue_construction', ['dominion_id' => $dominion->id, 'building' => 'home']);
     }
 }
