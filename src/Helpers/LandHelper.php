9c4cb0da167988fc28d144c03b9a454594a20732
WaveHack <email@wavehack.net>
2017-01-07 20:18:34 +0100

    Add building and land helpers

---
+++ src/Helpers/LandHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OpenDominion\Helpers;
+
+use OpenDominion\Models\Race;
+
+class LandHelper
+{
+    public static function getLandTypes()
+    {
+        return [
+            'plain' => 'Plains',
+            'mountain' => 'Mountains',
+            'swamp' => 'Swamp',
+            'cavern' => 'Caverns',
+            'forest' => 'Forests',
+            'hill' => 'Hills',
+            'water' => 'Water',
+        ];
+    }
+
+    public static function getLandTypeByBuilding($building, Race $race)
+    {
+        return []; // todo
+    }
+}
