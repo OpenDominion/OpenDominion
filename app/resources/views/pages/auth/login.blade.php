237e6920a4ad82377790a70b05f557144ac5d998
WaveHack <email@wavehack.net>
2017-01-08 00:51:00 +0100

    Update login view

--- app/resources/views/pages/auth/login.blade.php
+++ app/resources/views/pages/auth/login.blade.php
@@ -1,27 +1,59 @@
-@extends('layouts.master')
-
-@section('page-header', 'Login')
+@extends('layouts.topnav')
 
 @section('content')
     <div class="row">
-        <div class="col-sm-6 col-md-5 col-lg-4">
-            <form action="{{ route('auth.login') }}" method="post" role="form">
-                {{ csrf_field() }}
-                <fieldset>
-                    <div class="form-group">
-                        <input type="email" class="form-control" name="email" placeholder="Email" autofocus>
+        <div class="col-sm-8 col-sm-offset-2">
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Login</h3>
+                </div>
+                <form action="{{ route('auth.login') }}" method="post" class="form-horizontal" role="form">
+                    {{ csrf_field() }}
+
+                    <div class="box-body">
+
+                        <!-- Email -->
+                        <div class="form-group">
+                            <label for="email" class="col-sm-3 control-label">Email</label>
+                            <div class="col-sm-9">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="form-group">
+                            <label for="password" class="col-sm-3 control-label">Password</label>
+                            <div class="col-sm-9">
+                                <input type="password" name="password" id="password" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+
+                        <!-- Remember me -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-9">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> Remember me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Button -->
+
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Password">
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        <div class="pull-right">
+                            Don't have an account? <a href="{{ route('auth.register') }}">Register</a>
+                        </div>
                     </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember">Remember Me
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
-                </fieldset>
-            </form>
+
+                </form>
+            </div>
+
         </div>
     </div>
 @endsection
