53db61ee50f60fe4d518d21662b406988c0c14ff
WaveHack <email@wavehack.net>
2017-01-08 23:03:24 +0100

    Add incomplete tests for population calculator

---
+++ tests/Unit/Calculators/Dominion/PopulationCalculatorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace OpenDominion\Tests\Unit\Calculators\Dominion;
+
+use OpenDominion\Tests\BaseTestCase;
+
+class PopulationCalculatorTest extends BaseTestCase
+{
+    public function testGetMaxPopulation()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetMaxPopulationRaw()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetMaxPopulationMultiplier()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationBirth()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationBirthRaw()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationPeasantGrowth()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationDrafteeGrowth()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationPeasantPercentage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationMilitaryMaxTrainable()
+    {
+        $this->markTestIncomplete();
+    }
+}
