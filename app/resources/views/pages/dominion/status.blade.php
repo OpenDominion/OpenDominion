d24608e534d1b8915fde82befc44f21f181892a7
WaveHack <email@wavehack.net>
2017-01-08 16:39:23 +0100

    Fix dominion status view

--- app/resources/views/pages/dominion/status.blade.php
+++ app/resources/views/pages/dominion/status.blade.php
@@ -3,98 +3,90 @@
 @section('page-header', 'Status')
 
 @section('content')
-    {{ $selectedDominion }}
-
-    {{--<div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="ra ra-capitol ra-fw"></i> {{ $dominion->name }} (#{{ $dominion->round->number }} {{ $dominion->round->name }})
-                </div>
-                <div class="panel-body">
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th colspan="2">Overview</th>
-                                    <th colspan="2">Resources</th>
-                                    <th colspan="2">Military</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Ruler:</td>
-                                    <td>{{ Auth::user()->display_name }}</td>
-                                    <td>Platinum:</td>
-                                    <td>{{ number_format($dominion->resource_platinum) }}</td>
-                                    <td>Morale:</td>
-                                    <td>{{ $dominion->morale }}%</td>
-                                </tr>
-                                <tr>
-                                    <td>Race:</td>
-                                    <td>{{ $dominion->race->name }}</td>
-                                    <td>Food:</td>
-                                    <td>{{ number_format($dominion->resource_food) }}</td>
-                                    <td>Draftees:</td>
-                                    <td>{{ number_format($dominion->military_draftees) }}</td>
-                                </tr>
-                                <tr>
-                                    <td>Land:</td>
-                                    <td>NYI</td>
-                                    <td>Lumber:</td>
-                                    <td>{{ number_format($dominion->resource_lumber) }}</td>
-                                    <td>Unit 1:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td>Peasants:</td>
-                                    <td>{{ number_format($dominion->peasants) }}</td>
-                                    <td>Mana:</td>
-                                    <td>NYI</td>
-                                    <td>Unit 2:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td>Employment:</td>
-                                    <td>NYI</td>
-                                    <td>Ore:</td>
-                                    <td>NYI</td>
-                                    <td>Unit 3:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td>Networth:</td>
-                                    <td>{{ number_format($dominion->networth) }}</td>
-                                    <td>Gems:</td>
-                                    <td>NYI</td>
-                                    <td>Unit 4:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td>Prestige:</td>
-                                    <td>{{ number_format($dominion->prestige) }}</td>
-                                    <td>Research Points:</td>
-                                    <td>NYI</td>
-                                    <td>Spies:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2"></td>
-                                    <td>Boats:</td>
-                                    <td>NYI</td>
-                                    <td>Wizards:</td>
-                                    <td>NYI</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4"></td>
-                                    <td>ArchMages:</td>
-                                    <td>NYI</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="ra ra-capitol ra-fw"></i> The Dominion of {{ $selectedDominion->name }}</h3>
         </div>
-    </div>--}}
+        <div class="box-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th colspan="2">Overview</th>
+                        <th colspan="2">Resources</th>
+                        <th colspan="2">Military</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Ruler:</td>
+                        <td>{{ Auth::user()->display_name }}</td>
+                        <td>Platinum:</td>
+                        <td>{{ number_format($selectedDominion->resource_platinum) }}</td>
+                        <td>Morale:</td>
+                        <td>{{ $selectedDominion->morale }}%</td>
+                    </tr>
+                    <tr>
+                        <td>Race:</td>
+                        <td>{{ $selectedDominion->race->name }}</td>
+                        <td>Food:</td>
+                        <td>{{ number_format($selectedDominion->resource_food) }}</td>
+                        <td>Draftees:</td>
+                        <td>{{ number_format($selectedDominion->military_draftees) }}</td>
+                    </tr>
+                    <tr>
+                        <td>Land:</td>
+                        <td>NYI</td>
+                        <td>Lumber:</td>
+                        <td>{{ number_format($selectedDominion->resource_lumber) }}</td>
+                        <td>Unit 1:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td>Peasants:</td>
+                        <td>{{ number_format($selectedDominion->peasants) }}</td>
+                        <td>Mana:</td>
+                        <td>NYI</td>
+                        <td>Unit 2:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td>Employment:</td>
+                        <td>NYI</td>
+                        <td>Ore:</td>
+                        <td>NYI</td>
+                        <td>Unit 3:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td>Networth:</td>
+                        <td>{{ number_format($selectedDominion->networth) }}</td>
+                        <td>Gems:</td>
+                        <td>NYI</td>
+                        <td>Unit 4:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td>Prestige:</td>
+                        <td>{{ number_format($selectedDominion->prestige) }}</td>
+                        <td>Research Points:</td>
+                        <td>NYI</td>
+                        <td>Spies:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"></td>
+                        <td>Boats:</td>
+                        <td>NYI</td>
+                        <td>Wizards:</td>
+                        <td>NYI</td>
+                    </tr>
+                    <tr>
+                        <td colspan="4"></td>
+                        <td>ArchMages:</td>
+                        <td>NYI</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
