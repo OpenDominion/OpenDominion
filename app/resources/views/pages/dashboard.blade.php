3aeee19791e9f0d9d76bb88fb9039709d6dc2f38
WaveHack <email@wavehack.net>
2017-01-08 02:05:13 +0100

    Update dashboard view

--- app/resources/views/pages/dashboard.blade.php
+++ app/resources/views/pages/dashboard.blade.php
@@ -4,10 +4,124 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12">
-            <p>Welcome back, <b>{{ Auth::user()->display_name }}</b>.</p>
+        <div class="col-lg-6">
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="ra ra-capitol ra-fw"></i> Dominions</h3>
+                </div>
+                <div class="box-body">
+
+                    @if ($dominions->isEmpty())
+                        <p>You have no active dominions. Register in a round to create a dominion.</p>
+                    @else
+                        @foreach($dominions->chunk(3) as $dominion)
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    dom todo
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-6">
+
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-clock-o fa-fw"></i> Rounds</h3>
+                </div>
+
+                @if ($rounds->isEmpty())
+
+                    <div class="box-body">
+                        <p>There are currently no active rounds.</p>
+                    </div>
+
+                @else
+
+                    <div class="box-body no-padding">
+                        <table class="table">
+                            <colgroup>
+                                <col width="40">
+                                <col>
+                                <col width="80">
+                                <col width="00">
+                                <col width="80">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th class="text-center">#</th>
+                                    <th>Name</th>
+                                    <th class="text-center">Start</th>
+                                    <th class="text-center">Duration</th>
+                                    <th class="text-center">Register</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($rounds->all() as $round)
+                                    @php
+                                        $trClass = 'danger';
+                                        $userAlreadyRegistered = $round->userAlreadyRegistered(Auth::user());
+
+                                        if ($userAlreadyRegistered) {
+                                            $trClass = 'info';
+                                        } elseif ($round->hasStarted()) {
+                                            $trClass = 'warning';
+                                        } elseif ($round->openForRegistration()) {
+                                            $trClass = 'success';
+                                        }
+                                    @endphp
+
+                                    <tr class="{{ $trClass }}">
+                                        <td class="text-center">{{ $round->number }}</td>
+                                        <td>
+                                            {{ $round->name }}
+                                            <span class="text-muted">({{ $round->league->description }})</span>
+                                        </td>
+                                        <td class="text-center">
+                                            @if ($round->hasStarted())
+                                                <abbr class="text-warning" title="Started at {{ $round->start_date }}">Started!</abbr>
+                                                {{-- todo: Show current round milestone (mid, end etc) with appropriate text color --}}
+                                            @else
+                                                <abbr title="Starting at {{ $round->start_date }}">In {{ $round->daysUntilStart() }} day(s)</abbr>
+                                            @endif
+                                        </td>
+                                        <td class="text-center">
+                                            <abbr title="Ending at {{ $round->end_date }}">{{ $round->durationInDays() }} days</abbr>
+                                        </td>
+                                        <td class="text-center">
+                                            @if ($userAlreadyRegistered)
+                                                Already registered!
+                                            @elseif ($round->openForRegistration())
+                                                <a href="{{ route('round.register', $round) }}" class="btn btn-primary btn-flat btn-xs">Register</a>
+                                            @else
+                                                In {{ $round->daysUntilRegistration() }} day(s)
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                @endif
+
+            </div>
+
         </div>
     </div>
+
+
+    <p>space</p>
+    <p>space</p>
+    <p>space</p>
+
+
+
+
 
     <div class="row">
         <div class="col-lg-12">
@@ -61,85 +175,5 @@
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-clock-o fa-fw"></i> Rounds
-                </div>
-                <div class="panel-body">
-                    @if ($rounds->isEmpty())
-                        <p>There are currently no active rounds.</p>
-                    @else
-                        <div class="table-responsive">
-                            <table class="table table-hover table-striped">
-                                <colgroup>
-                                    <col width="40">
-                                    <col>
-                                    <col width="120">
-                                    <col width="120">
-                                    <col width="120">
-                                </colgroup>
-                                <thead>
-                                    <tr>
-                                        <th class="text-center">#</th>
-                                        <th>Name</th>
-                                        <th class="text-center">Start</th>
-                                        <th class="text-center hidden-xs">Duration</th>
-                                        <th class="text-center">Register</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($rounds->all() as $round)
-                                        @php
-                                            $trClass = 'danger';
-                                            $userAlreadyRegistered = $round->userAlreadyRegistered(Auth::user());
-
-                                            if ($userAlreadyRegistered) {
-                                                $trClass = 'info';
-                                            } elseif ($round->hasStarted()) {
-                                                $trClass = 'warning';
-                                            } elseif ($round->openForRegistration()) {
-                                                $trClass = 'success';
-                                            }
-                                        @endphp
-                                        <tr class="{{ $trClass }}">
-                                            <td class="text-center">{{ $round->number }}</td>
-                                            <td>
-                                                {{ $round->name }}
-                                                <span class="text-muted">({{ $round->league->description }})</span>
-                                            </td>
-                                            <td class="text-center">
-                                                @if ($round->hasStarted())
-                                                    <abbr class="text-warning" title="Started at {{ $round->start_date }}">Started!</abbr>
-                                                    {{-- todo: Show current round milestone (mid, end etc) with appropriate text color --}}
-                                                @else
-                                                    <abbr title="Starting at {{ $round->start_date }}">In {{ $round->daysUntilStart() }} day(s)</abbr>
-                                                @endif
-                                            </td>
-                                            <td class="text-center hidden-xs">
-                                                <abbr title="Ending at {{ $round->end_date }}">{{ $round->durationInDays() }} days</abbr>
-                                            </td>
-                                            <td class="text-center">
-                                                @if ($userAlreadyRegistered)
-                                                    Already registered!
-                                                @elseif ($round->openForRegistration())
-                                                    <a href="{{ route('round.register', $round) }}" class="btn btn-primary btn-xs">Register</a>
-                                                @else
-                                                    In {{ $round->daysUntilRegistration() }} day(s)
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-
     </div>
 @endsection
