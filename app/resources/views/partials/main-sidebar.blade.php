6cf0b3a99d6f9dcfae3be427b83180e5b21cb314
WaveHack <email@wavehack.net>
2017-01-08 01:34:20 +0100

    Update sidebar

--- app/resources/views/partials/main-sidebar.blade.php
+++ app/resources/views/partials/main-sidebar.blade.php
@@ -1,7 +1,17 @@
 <aside class="main-sidebar">
     <section class="sidebar">
 
-        <!-- Sidebar user panel -->
+        <ul class="sidebar-menu">
+            @if(0) {{--dominion selected--}}
+
+            @else
+
+            @endif
+
+            <li class="{{ Route::is('dashboard') ? 'active' : null }}"><a href="#"><i class="fa fw-dashboard"></i> Dashboard</a></li>
+        </ul>
+
+{{--        <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
                 <img src="http://placehold.it/160x160" class="img-circle" alt="User Image">
@@ -17,7 +27,7 @@
             <li class="header">HEADER</li>
             <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
             <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-        </ul>
+        </ul>--}}
 
     </section>
 </aside>
