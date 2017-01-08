707a7ab5efdcbc57d619f72004c59e7a942023c3
WaveHack <email@wavehack.net>
2017-01-08 22:54:00 +0100

    Add production calculator test

---
+++ tests/Unit/Calculators/Dominion/ProductionCalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OpenDominion\Tests\Unit\Calculators\Dominion;
+
+use OpenDominion\Tests\BaseTestCase;
+
+class ProductionCalculatorTest extends BaseTestCase
+{
+    public function getPlatinumProduction()
+    {
+        $this->markTestIncomplete();
+    }
+}
