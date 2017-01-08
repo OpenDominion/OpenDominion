42d81efcbfe375ebef2f4cca62ba888ed98171e7
WaveHack <email@wavehack.net>
2017-01-08 17:39:57 +0100

    Text update

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
                     <h3 class="box-title">Welcome to OpenDominion!</h3>
                 </div>
                 <div class="box-body">
-                    <p>OpenDominion is a free online text-based strategy game in a medieval fantasy setting. You control a kingdom called a 'dominion', along with its resources, buildings, land and units. You are placed in a realm with up to 14 other dominions and you must work together to make your realm the wealthiest and most powerful in the current round!</p>
+                    <p>OpenDominion is a free online text-based strategy game in a medieval fantasy setting. You control a kingdom called a 'dominion', along with its resources, buildings, land and units. You are placed in a realm with up to 11 other dominions and you must work together to make your realm the wealthiest and most powerful in the current round!</p>
 
                     <p>To start playing, <a href="{{ route('auth.register') }}">register</a> an account and sign up for a round after registration. If you already have an account, <a href="{{ route('auth.login') }}">login</a> instead.</p>
 
