0fc0988b3b08e4192b4311fcd51271702ebc7050
WaveHack <email@wavehack.net>
2017-01-10 18:57:49 +0100

    Update building helper

--- src/Helpers/BuildingHelper.php
+++ src/Helpers/BuildingHelper.php
@@ -4,28 +4,28 @@ namespace OpenDominion\Helpers;
 
 class BuildingHelper
 {
-    public static function getBuildingTypes()
+    public function getBuildingTypes()
     {
         return [
             'home' => 'Home',
             'alchemy' => 'Alchemy',
             'farm' => 'Farm',
-            'smithy' => 'Smithy',
-            'masonry' => 'Masonry',
-            'ore_mine' => 'Ore Mine',
-            'gryphon_nest' => 'Gryphon Nest',
-            'tower' => 'Tower',
-            'wizard_guild' => 'Wizard Guild',
-            'temple' => 'Temple',
-            'diamond_mine' => 'Diamond Mine',
-            'school' => 'School',
+//            'smithy' => 'Smithy',
+//            'masonry' => 'Masonry',
+//            'ore_mine' => 'Ore Mine',
+//            'gryphon_nest' => 'Gryphon Nest',
+//            'tower' => 'Tower',
+//            'wizard_guild' => 'Wizard Guild',
+//            'temple' => 'Temple',
+//            'diamond_mine' => 'Diamond Mine',
+//            'school' => 'School',
             'lumberyard' => 'Lumberyard',
-            'forest_haven' => 'Forest Haven',
-            'factory' => 'Factory',
-            'guard_tower' => 'Guard Tower',
-            'shrine' => 'Shrine',
+//            'forest_haven' => 'Forest Haven',
+//            'factory' => 'Factory',
+//            'guard_tower' => 'Guard Tower',
+//            'shrine' => 'Shrine',
             'barracks' => 'Barracks',
-            'dock' => 'Dock',
+//            'dock' => 'Dock',
         ];
     }
 }
