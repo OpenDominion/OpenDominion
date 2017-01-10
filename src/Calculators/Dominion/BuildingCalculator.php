9349d312f3872c564ae04299464a621f6249fc8c
WaveHack <email@wavehack.net>
2017-01-10 21:16:37 +0100

    Implement getTotalBuildings

--- src/Calculators/Dominion/BuildingCalculator.php
+++ src/Calculators/Dominion/BuildingCalculator.php
@@ -2,14 +2,34 @@
 
 namespace OpenDominion\Calculators\Dominion;
 
+use OpenDominion\Helpers\BuildingHelper;
 use OpenDominion\Traits\DominionAwareTrait;
 
 class BuildingCalculator
 {
     use DominionAwareTrait;
 
+    /** @var BuildingHelper */
+    protected $buildingHelper;
+
+    public function __construct(BuildingHelper $buildingHelper)
+    {
+        $this->buildingHelper = $buildingHelper;
+    }
+
+    /**
+     * Returns the Dominion' total number of buildings.
+     *
+     * @return int
+     */
     public function getTotalBuildings()
     {
-        return 0;
+        $totalBuildings = 0;
+
+        foreach (array_keys($this->buildingHelper->getBuildingTypes()) as $buildingType) {
+            $totalBuildings += $this->dominion->{'building_' . $buildingType};
+        }
+
+        return $totalBuildings;
     }
 }
--- tests/Unit/Calculators/Dominion/BuildingCalculatorTest.php
+++ tests/Unit/Calculators/Dominion/BuildingCalculatorTest.php
@@ -2,12 +2,27 @@
 
 namespace OpenDominion\Tests\Unit\Calculators\Dominion;
 
+use CoreDataSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use OpenDominion\Calculators\Dominion\BuildingCalculator;
+use OpenDominion\Models\Race;
 use OpenDominion\Tests\BaseTestCase;
 
 class BuildingCalculatorTest extends BaseTestCase
 {
+    use DatabaseMigrations;
+
     public function testGetTotalBuildings()
     {
-        $this->markTestIncomplete();
+        $this->seed(CoreDataSeeder::class);
+        $user = $this->createUser();
+        $round = $this->createRound();
+        $race = Race::where('alignment', 'good')->firstOrFail();
+        $dominion = $this->createDominion($user, $round, $race);
+
+        $buildingCalculator = $this->app->make(BuildingCalculator::class)
+            ->setDominion($dominion);
+
+        $this->assertEquals(90, $buildingCalculator->getTotalBuildings());
     }
 }
