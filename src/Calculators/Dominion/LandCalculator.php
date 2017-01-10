3215d5ea44e04e48421b0799d5b965a581df3de1
WaveHack <email@wavehack.net>
2017-01-10 23:29:50 +0100

    Implement getTotalLand

--- src/Calculators/Dominion/LandCalculator.php
+++ src/Calculators/Dominion/LandCalculator.php
@@ -2,15 +2,35 @@
 
 namespace OpenDominion\Calculators\Dominion;
 
+use OpenDominion\Helpers\LandHelper;
 use OpenDominion\Traits\DominionAwareTrait;
 
 class LandCalculator
 {
     use DominionAwareTrait;
 
+    /** @var LandHelper */
+    protected $landHelper;
+
+    public function __construct(LandHelper $landHelper)
+    {
+        $this->landHelper = $landHelper;
+    }
+
+    /**
+     * Returns the Dominion's total acres of land.
+     *
+     * @return int
+     */
     public function getTotalLand()
     {
-        return 0;
+        $totalLand = 0;
+
+        foreach (array_keys($this->landHelper->getLandTypes()) as $landType) {
+            $totalLand += $this->dominion->{'land_' . $landType};
+        }
+
+        return $totalLand;
     }
 
     public function getTotalBarrenLand()
--- tests/Unit/Calculators/Dominion/LandCalculatorTest.php
+++ tests/Unit/Calculators/Dominion/LandCalculatorTest.php
@@ -2,13 +2,49 @@
 
 namespace OpenDominion\Tests\Unit\Calculators\Dominion;
 
+use Mockery as m;
+use OpenDominion\Calculators\Dominion\LandCalculator;
+use OpenDominion\Models\Dominion;
 use OpenDominion\Tests\BaseTestCase;
 
 class LandCalculatorTest extends BaseTestCase
 {
+    /** @var Dominion */
+    protected $dominion;
+
+    /** @var LandCalculator */
+    protected $landCalculator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->dominion = m::mock(Dominion::class);
+
+        $this->landCalculator = $this->app->make(LandCalculator::class)
+            ->setDominion($this->dominion);
+    }
+
     public function testGetTotalLand()
     {
-        $this->markTestIncomplete();
+        $landTypes = [
+            'plain',
+            'mountain',
+            'swamp',
+            'cavern',
+            'forest',
+            'hill',
+            'water',
+        ];
+
+        $expected = 0;
+
+        for ($i = 0, $countLandTypes = count($landTypes); $i < $countLandTypes; ++$i) {
+            $this->dominion->shouldReceive('getAttribute')->with("land_{$landTypes[$i]}")->andReturn(1 << $i);
+            $expected += (1 << $i);
+        }
+
+        $this->assertEquals($expected, $this->landCalculator->getTotalLand());
     }
 
     public function testGetTotalBarrenLand()
