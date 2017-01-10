80fb39fe1bb7e539e3683f2abd72be25354964c8
WaveHack <email@wavehack.net>
2017-01-10 19:05:24 +0100

    Add building and land calculator test stubs

---
+++ tests/Unit/Calculators/Dominion/BuildingCalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OpenDominion\Tests\Unit\Calculators\Dominion;
+
+use OpenDominion\Tests\BaseTestCase;
+
+class BuildingCalculatorTest extends BaseTestCase
+{
+    public function testGetTotalBuildings()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Unit/Calculators/Dominion/LandCalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OpenDominion\Tests\Unit\Calculators\Dominion;
+
+use OpenDominion\Tests\BaseTestCase;
+
+class LandCalculatorTest extends BaseTestCase
+{
+    public function testGetTotalLand()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetTotalBarrenLand()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetBarrenLandByLandType()
+    {
+        $this->markTestIncomplete();
+    }
+}
