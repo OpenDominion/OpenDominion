cb2a3e3b82fc95001aa8b4e186cf4559f5ce6ab4
WaveHack <email@wavehack.net>
2017-01-12 00:50:57 +0100

    Add employment calculator test stub

---
+++ tests/Unit/Calculators/Dominion/EmploymentCalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OpenDominion\Tests\Unit\Calculators\Dominion;
+
+use OpenDominion\Tests\BaseTestCase;
+
+class EmploymentCalculatorTest extends BaseTestCase
+{
+    public function testGetEploymentJobs()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetPopulationEmployed()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetEmploymentPercentage()
+    {
+        $this->markTestIncomplete();
+    }
+}
