0b857b503909f4860871a701ba0f6952ec472510
WaveHack <email@wavehack.net>
2017-01-11 16:14:42 +0100

    Add getBuildingTypesByLandType

--- src/Helpers/BuildingHelper.php
+++ src/Helpers/BuildingHelper.php
@@ -2,6 +2,8 @@
 
 namespace OpenDominion\Helpers;
 
+use OpenDominion\Models\Race;
+
 class BuildingHelper
 {
     public function getBuildingTypes()
@@ -27,5 +29,49 @@ class BuildingHelper
             'barracks' => 'Barracks',
 //            'dock' => 'Dock',
         ];
+    }
+
+    public function getBuildingTypesByLandType(Race $race = null)
+    {
+        $return = [
+            'plain' => [
+                'alchemy',
+                'farm',
+//                'smithy',
+//                'masonry',
+            ],
+            'mountain' => [
+//                'ore_mine',
+//                'gryphon_nest',
+            ],
+            'swarmp' => [
+//                'tower',
+//                'wizard_guild',
+//                'template',
+            ],
+            'cavern' => [
+//                'diamond_mine',
+//                'school',
+            ],
+            'forest' => [
+                'lumberyard',
+//                'forest_haven',
+            ],
+            'hill' => [
+//                'factory',
+//                'guard_tower',
+//                'shrine',
+                'barracks',
+            ],
+            'water' => [
+//                'dock',
+            ],
+        ];
+
+        if ($race !== null) {
+            array_unshift($return[$race->home_land_type], 'home');
+        }
+
+        return $return;
     }
 }
