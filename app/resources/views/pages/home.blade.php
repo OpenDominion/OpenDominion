63a7bd9611f80840f224ba609455e47611a111f1
WaveHack <email@wavehack.net>
2017-01-16 13:14:21 +0100

    Link to original Dominion game over message

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -13,7 +13,7 @@
 
                     <p>To start playing, <a href="{{ route('auth.register') }}">register</a> an account and sign up for a round after registration. If you already have an account, <a href="{{ route('auth.login') }}">login</a> instead.</p>
 
-                    <p>OpenDominion is based on Dominion from Kamikaze Games. Dominion ran from about 2000 to 2012 until stopping indefinitely.</p>
+                    <p>OpenDominion is based on Dominion from Kamikaze Games. Dominion ran from about 2000 to 2012 until <a href="http://omgn.com/blog/cjrector/2012/06/21/dominion-r-i-p" target="_blank">stopping indefinitely <i class="fa fa-external-link"></i></a>.</p>
 
                     <p>OpenDominion is open source software and can be found on <a href="https://github.com/WaveHack/OpenDominion" target="_blank">GitHub <i class="fa fa-external-link"></i></a>.</p>
                 </div>
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@
 
 ## Introduction
 
-OpenDominion is an online text-based strategy game based on Dominion, a defunct online text-based strategy game from Kamikaze Games which ran from about 2000 to 2012.
+OpenDominion is an online text-based strategy game based on Dominion, a defunct online text-based strategy game from Kamikaze Games which ran from about 2000 to 2012 until [stopping indefinitely](http://omgn.com/blog/cjrector/2012/06/21/dominion-r-i-p).
 
 The goal of OpenDominion is to be an open source clone of Dominion, so that players can keep playing the game and developers can contribute and collaborate to develop and improve the game.
 
