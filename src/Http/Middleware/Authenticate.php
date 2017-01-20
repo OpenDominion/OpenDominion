b429afd8fa9bacb3715a50329c5ad6c344525fbc
WaveHack <email@wavehack.net>
2017-01-20 20:06:01 +0100

    Track GA anonymized user id

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace OpenDominion\Http\Middleware;
 
+use Analytics;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,12 +28,17 @@ class Authenticate
             }
         }
 
-        if (!$auth->user()->activated) {
+        $user = $auth->user();
+
+        if (!$user->activated) {
             $auth->logout();
             // todo: add "click here to have a new activation email being sent to you"
             $request->session()->flash('alert-danger', 'Your account has not been activated yet. Check your spam folder for the activation email.');
             return redirect()->guest(route('auth.login'));
         }
+
+        // Analytics
+        Analytics::setUserId(md5(env('APP_KEY') . $user->id));
 
         return $next($request);
     }
