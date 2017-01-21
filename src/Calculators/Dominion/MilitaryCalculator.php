f6ac4cd6801241efeb55de64ca56938cbca670ea
WaveHack <email@wavehack.net>
2017-01-21 22:56:36 +0100

    Implement raw DP/OP

--- src/Calculators/Dominion/MilitaryCalculator.php
+++ src/Calculators/Dominion/MilitaryCalculator.php
@@ -36,11 +36,18 @@ class MilitaryCalculator extends AbstractDominionCalculator
 
     public function getOffensivePowerRaw()
     {
-        // =E3*Overview!$B$22+F3*Overview!$B$23+G3*(VLOOKUP(Overview!$A$24,Races!$D$4:$H$108,2,FALSE)+$CH$7*BY3)+H3*(VLOOKUP(Overview!$A$25,Races!$D$4:$H$108,2,FALSE)+$CH$9*CD3+$CH$4*BU3)+$CH$8*CB3
+        $op = 0;
+
+        foreach ($this->dominion->race->units as $unit) {
+            $op += $unit->power_offense;
+        }
+
+        return (float)$op;
     }
 
     public function getOffensivePowerMultiplier()
     {
+        return 1; // todo
         // =Overview!$I$25+Imps!AB3+ROUND(MIN(Constants!$B$12*Construction!BA3/Construction!$E3,Constants!$D$12),4)+MAX(IF(Magic!$AE3>0,Constants!$F$79),IF(Magic!AG3>0,Constants!$F$81),IF(Magic!AJ3>0,Constants!$F$84)) + ROUNDDOWN(Production!O3/250*Constants!$B$90,2)/100 + MAX(Constants!$M$41*Techs!AH3,Constants!$M$55*Techs!AV3)
     }
 
@@ -61,12 +68,33 @@ class MilitaryCalculator extends AbstractDominionCalculator
 
     public function getDefensivePowerRaw()
     {
-        // =E3*Overview!$C$22+F3*Overview!$C$23+G3*(VLOOKUP(Overview!$A$24,Races!$D$4:$H$108,3,FALSE)+$CH$4*BT3+$CH$5*BW3+$CH$6*BX3+$CH$7*BZ3+$CH$9*CC3+$CH$10*CE3+$CH$11*CF3)+H3*(VLOOKUP(Overview!$A$25,Races!$D$4:$H$108,3,FALSE)+$CH$4*BV3) + MIN(Population!C3*Constants!B$13,Constants!B$13*20*Construction!AY3)+$CH$8*CA3
-        // + draftees * dp_per_draftee (1)
+        $dp = 0;
+
+        // Values
+        $dpPerDraftee = 1;
+        $forestHavenDpPerPeasant = 0.75;
+        $peasantsPerForestHaven = 20;
+
+        // Military
+        foreach ($this->dominion->race->units as $unit) {
+            $dp += $unit->power_defense;
+        }
+
+        // Draftees
+        $dp += ($this->dominion->military_draftees * $dpPerDraftee);
+
+        // Forest Havens
+        $dp += min(
+            ($this->dominion->peasants * $forestHavenDpPerPeasant),
+            ($this->dominion->building_forest_haven * $forestHavenDpPerPeasant * $peasantsPerForestHaven)
+        );
+
+        return (float)$dp;
     }
 
     public function getDefensivePowerMultiplier()
     {
+        return 1; // todo
         // =Overview!$I$24 + Imps!AC3 + ROUND( MIN(Constants!$B$11*Construction!BC3/Construction!$E3,Constants!$D$11), 4) + IF(Magic!AD3>0,Constants!$F$78,IF(Magic!AH3>0,Constants!$F$82,IF(Magic!AG3>0,Constants!$H$81,IF(Magic!Z3>0,Constants!$F$74))))
     }
 
