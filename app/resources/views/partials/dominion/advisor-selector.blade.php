0c4e955aa9ccdb1d7260ba992435160694f9ca49
WaveHack <email@wavehack.net>
2017-01-23 23:30:21 +0100

    Comment out military advisor for now

--- app/resources/views/partials/dominion/advisor-selector.blade.php
+++ app/resources/views/partials/dominion/advisor-selector.blade.php
@@ -9,20 +9,21 @@
                 <a href="{{ route('dominion.advisors.production') }}" class="btn btn-app btn-block">
                     <i class="fa fa-industry"></i> Production
                 </a>
-                <div class="spacer"></div>
             </div>
 
-            <div class="col-xs-6 col-sm-3">
+            {{--<div class="col-xs-6 col-sm-3">
                 <a href="{{ route('dominion.advisors.military') }}" class="btn btn-app btn-block">
                     <i class="ra ra-crossed-swords"></i> Military
                 </a>
-            </div>
+            </div>--}}
 
             <div class="col-xs-6 col-sm-3">
                 <a href="{{ route('dominion.advisors.land') }}" class="btn btn-app btn-block">
                     <i class="ra ra-honeycomb"></i> Land
                 </a>
             </div>
+
+            <div class="visible-xs">&nbsp;</div>
 
             <div class="col-xs-6 col-sm-3">
                 <a href="{{ route('dominion.advisors.construction') }}" class="btn btn-app btn-block">
