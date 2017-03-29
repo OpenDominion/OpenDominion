5dce295ea63d56848700a249df50c90681d41a0f
WaveHack <email@wavehack.net>
2017-03-29 22:23:24 +0200

    Refactor web routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,27 +3,27 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-$router->get('/', ['as' => 'home', 'uses' => 'HomeController@getIndex']);
+$router->get('/')->uses('HomeController@getIndex')->name('home');
 
 // Authentication
 
-$router->group(['prefix' => 'auth'], function (Router $router) {
+$router->group(['prefix' => 'auth', 'as' => 'auth.'], function (Router $router) {
 
     $router->group(['middleware' => 'guest'], function (Router $router) {
 
-        $router->get('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@getLogin']);
-        $router->post('login', 'Auth\LoginController@postLogin');
+        $router->get('login')->uses('Auth\LoginController@getLogin')->name('login');
+        $router->post('login')->uses('Auth\LoginController@postLogin');
 
-        $router->get('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@getRegister']);
-        $router->post('register', 'Auth\RegisterController@postRegister');
+        $router->get('register')->uses('Auth\RegisterController@getRegister')->name('register');
+        $router->post('register')->uses('Auth\RegisterController@postRegister');
 
-        $router->get('activate/{activation_code}', ['as' => 'auth.activate', 'uses' => 'Auth\RegisterController@getActivate']);
+        $router->get('activate/{activation_code}')->uses('Auth\RegisterController@getActivate')->name('activate');
 
     });
 
     $router->group(['middleware' => 'auth'], function (Router $router) {
 
-        $router->post('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@postLogout']);
+        $router->post('logout')->uses('Auth\LoginController@postLogout')->name('logout');
 
     });
 
@@ -33,39 +33,39 @@ $router->group(['prefix' => 'auth'], function (Router $router) {
 
 $router->group(['middleware' => 'auth'], function (Router $router) {
 
-    $router->get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@getIndex']);
+    $router->get('dashboard')->uses('DashboardController@getIndex')->name('dashboard');
 
-    $router->get('round/{round}/register', ['as' => 'round.register', 'uses' => 'RoundController@getRegister']);
-    $router->post('round/{round}/register', 'RoundController@postRegister');
+    $router->get('round/{round}/register')->uses('RoundController@getRegister')->name('round.register');
+    $router->post('round/{round}/register')->uses('RoundController@postRegister');
 
-    $router->group(['prefix' => 'dominion'], function (Router $router) {
+    $router->group(['prefix' => 'dominion', 'as' => 'dominion.'], function (Router $router) {
 
-        $router->get('{dominion}/select', function () { return redirect()->route('dashboard'); });
-        $router->post('{dominion}/select', ['as' => 'dominion.select', 'uses' => 'DominionController@postSelect']);
+//        $router->get('{dominion}/select')->uses(function () { return redirect()->route('dashboard'); });
+        $router->post('{dominion}/select')->uses('DominionController@postSelect')->name('select');
 
         $router->group(['middleware' => 'dominionselected'], function (Router $router) {
 
             // Other Dominion actions
 
-            $router->get('realm/{realm}', ['as' => 'dominion.other.realm', 'uses' => 'DominionController@getRealm']);
+            $router->get('realm/{realm}')->uses('DominionController@getRealm')->name('other.realm');
 //            $router->get('{dominion}/status', ['as' => 'dominion.other.status', 'uses' => 'DominionController@getOtherStatus']);
 
             // Dominion
 
-            $router->get('status', ['as' => 'dominion.status', 'uses' => 'DominionController@getStatus']);
-            $router->get('advisors', ['as' => 'dominion.advisors', 'uses' => 'DominionController@getAdvisors']);
-            $router->get('advisors/production', ['as' => 'dominion.advisors.production', 'uses' => 'DominionController@getAdvisorsProduction']);
-            $router->get('advisors/military', ['as' => 'dominion.advisors.military', 'uses' => 'DominionController@getAdvisorsMilitary']);
-            $router->get('advisors/land', ['as' => 'dominion.advisors.land', 'uses' => 'DominionController@getAdvisorsLand']);
-            $router->get('advisors/construction', ['as' => 'dominion.advisors.construction', 'uses' => 'DominionController@getAdvisorsConstruction']);
+            $router->get('status')->uses('DominionController@getStatus')->name('status');
+            $router->get('advisors')->uses('DominionController@getAdvisors')->name('advisors');
+            $router->get('advisors/production')->uses('DominionController@getAdvisorsProduction')->name('production');
+            $router->get('advisors/military')->uses('DominionController@getAdvisorsMilitary')->name('military');
+            $router->get('advisors/land')->uses('DominionController@getAdvisorsLand')->name('land');
+            $router->get('advisors/construction')->uses('DominionController@getAdvisorsConstruction')->name('construction');
 
             // Actions
 
-            $router->get('explore', ['as' => 'dominion.explore', 'uses' => 'DominionController@getExplore']);
-            $router->post('explore', 'DominionController@postExplore');
+            $router->get('explore')->uses('DominionController@getExplore')->name('explore');
+            $router->post('explore')->uses('DominionController@postExplore');
 
-            $router->get('construction', ['as' => 'dominion.construction', 'uses' => 'DominionController@getConstruction']);
-            $router->post('construction', 'DominionController@postConstruction');
+            $router->get('construction')->uses('DominionController@getConstruction')->name('construction');
+            $router->post('construction')->uses('DominionController@postConstruction');
 
 //            $router->get('rezone-land', ['as' => 'dominion.rezone-land', 'uses' => 'DominionController@getRezoneLand']);
 //            $router->get('improvements', ['as' => 'dominion.improvements', 'uses' => 'DominionController@getImprovements']);
@@ -77,7 +77,7 @@ $router->group(['middleware' => 'auth'], function (Router $router) {
 
             // Realm
 
-            $router->get('realm', ['as' => 'dominion.realm', 'uses' => 'DominionController@getRealm']);
+            $router->get('realm')->uses('DominionController@getRealm')->name('realm');
             // todo: post/change realm
 
             // Misc?
