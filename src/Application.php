988d23bca04f5c65e898ee99196ec2e6006e84e8
WaveHack <email@wavehack.net>
2017-04-29 03:11:31 +0200

    Add docs and update constructor signature

--- src/Application.php
+++ src/Application.php
@@ -8,7 +8,12 @@ class Application extends LaravelApplication
 {
     protected $appPath;
 
-    public function __construct($basePath)
+    /**
+     * Create a new OpenDominion application instance.
+     *
+     * @param string|null $basePath
+     */
+    public function __construct($basePath = null)
     {
         parent::__construct($basePath);
 
@@ -16,36 +21,60 @@ class Application extends LaravelApplication
         $this->bindPathsInContainer();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function path($path = '')
     {
         return ($this->basePath . DIRECTORY_SEPARATOR . 'src' . $this->getSuffixPath($path));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function bootstrapPath($path = '')
     {
         return ($this->appPath . DIRECTORY_SEPARATOR . 'bootstrap' . $this->getSuffixPath($path));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function configPath($path = '')
     {
         return ($this->appPath . DIRECTORY_SEPARATOR . 'config' . $this->getSuffixPath($path));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function databasePath($path = '')
     {
         return ($this->appPath . DIRECTORY_SEPARATOR . 'database' . $this->getSuffixPath($path));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function langPath()
     {
         return ($this->appPath . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'lang');
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function storagePath()
     {
         return ($this->appPath . DIRECTORY_SEPARATOR . 'storage');
     }
 
+    /**
+     * Get path prefixed by directory separator if not empty.
+     *
+     * @param string $path
+     * @return string
+     */
     protected function getSuffixPath($path = '')
     {
         return ($path ? (DIRECTORY_SEPARATOR . $path) : '');
