8e86010290099982831102e434ecdb28ebdce2c7
WaveHack <email@wavehack.net>
2017-05-03 15:18:55 +0200

    Update login failed message

--- app/resources/lang/en/auth.php
+++ app/resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'These credentials do not match our records. If you registered before, you can re-register since the database has been reset due to development purposes.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
