d325b2abc1a4488e8bc9dd483ec014d780c6e06e
WaveHack <email@wavehack.net>
2017-07-28 22:49:11 +0200

    Add phpdoc to rezoning calculator contract

--- src/Contracts/Calculators/Dominion/Actions/RezoningCalculator.php
+++ src/Contracts/Calculators/Dominion/Actions/RezoningCalculator.php
@@ -6,7 +6,19 @@ use OpenDominion\Models\Dominion;
 
 interface RezoningCalculator
 {
+    /**
+     * Returns the Dominion's rezoning platinum cost (per acre of land).
+     *
+     * @param Dominion $dominion
+     * @return int
+     */
     public function getPlatinumCost(Dominion $dominion);
 
+    /**
+     * Returns the maximum number of acres of land a Dominion can rezone.
+     *
+     * @param Dominion $dominion
+     * @return int
+     */
     public function getMaxAfford(Dominion $dominion);
 }
