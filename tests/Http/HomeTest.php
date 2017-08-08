1978a27574370d2fd34bf236f7783e97eea1472f
WaveHack <email@wavehack.net>
2017-08-09 01:57:36 +0200

    Add HomeTest

---
+++ tests/Http/HomeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OpenDominion\Tests\Http;
+
+use CoreDataSeeder;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use OpenDominion\Tests\AbstractHttpTestCase;
+
+class HomeTest extends AbstractHttpTestCase
+{
+    use DatabaseMigrations;
+
+    public function testIndex()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testRedirectLoggedWithoutSelectedDominionToDashboard()
+    {
+        $user = $this->createAndImpersonateUser();
+
+        $this->actingAs($user)
+            ->get('/')
+            ->assertRedirect('/dashboard');
+    }
+
+    public function testRedirectLoggedWithSelectedDominionToStatus()
+    {
+        $this->seed(CoreDataSeeder::class);
+        $user = $this->createAndImpersonateUser();
+        $round = $this->createRound();
+        $this->createAndSelectDominion($user, $round);
+
+        $this->actingAs($user)
+            ->get('/')
+            ->assertRedirect('/dominion/status');
+    }
+}
