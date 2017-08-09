8d462fb45f811eaeb56df6ea5d22c4f9c80d7e20
WaveHack <email@wavehack.net>
2017-08-09 17:30:51 +0200

    Add link to Valhalla in topnav

--- app/resources/views/layouts/topnav.blade.php
+++ app/resources/views/layouts/topnav.blade.php
@@ -35,8 +35,8 @@
                 <!-- Navbar Left Menu -->
                 <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="{{ Route::is('home') ? 'active' : null }}"><a href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a></li>
-                        {{--<li><a href="#">About</a></li>--}}
+                        <li class="{{ Route::is('home') ? 'active' : null }}"><a href="{{ route('home') }}">Home</a></li>
+                        <li class="{{ Route::is('valhalla.*') ? 'active' : null }}"><a href="{{ route('valhalla.index') }}">Valhalla</a></li>
                     </ul>
                 </div>
 
