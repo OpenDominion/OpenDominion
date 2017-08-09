1b2fab9648c2fdab4e1e3d24d78a9d70d4ff7319
WaveHack <email@wavehack.net>
2017-08-09 23:44:35 +0200

    Add empty check to Valhalla

--- app/resources/views/pages/valhalla/round-type.blade.php
+++ app/resources/views/pages/valhalla/round-type.blade.php
@@ -7,40 +7,45 @@
         <div class="box-header with-border">
             <h3 class="box-title">{{ ucwords(str_replace('-', ' ', $type)) }} in round {{ number_format($round->number) }}: {{ $round->name }}</h3>
         </div>
-        <div class="box-body no-padding">
-            @php($headersPrinted = false)
-            <table class="table">
-                <colgroup>
-                    @foreach (array_keys($data[0]) as $column)
-                        <col{!! isset($headers[$column]['width']) ? (' width="' . $headers[$column]['width'] . '"') : null !!}>
-                    @endforeach
-                </colgroup>
-                @foreach ($data as $row)
-                    @if (!$headersPrinted)
-                        <thead>
-                            <tr>
-                                @foreach (array_keys($row) as $column)
-                                    <th{!! (isset($headers[$column]['align-center']) && $headers[$column]['align-center']) ? ' class="text-center"' : null !!}>
-                                        {{ ucwords($column) }}
-                                    </th>
-                                @endforeach
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @php($headersPrinted = true)
-                    @endif
 
-                    <tr>
-                        @foreach ($row as $column => $value)
-                            <td{!! (isset($headers[$column]['align-center']) && $headers[$column]['align-center']) ? ' class="text-center"' : null !!}>
-                                {{ $value }}
-                            </td>
+        @if (!empty($data))
+            <div class="box-body no-padding">
+                @php($headersPrinted = false)
+                <table class="table">
+                    <colgroup>
+                        @foreach (array_keys($data[0]) as $column)
+                            <col{!! isset($headers[$column]['width']) ? (' width="' . $headers[$column]['width'] . '"') : null !!}>
                         @endforeach
-                    </tr>
-                @endforeach
+                    </colgroup>
+                    @foreach ($data as $row)
+                        @if (!$headersPrinted)
+                            <thead>
+                                <tr>
+                                    @foreach (array_keys($row) as $column)
+                                        <th{!! (isset($headers[$column]['align-center']) && $headers[$column]['align-center']) ? ' class="text-center"' : null !!}>
+                                            {{ ucwords($column) }}
+                                        </th>
+                                    @endforeach
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @php($headersPrinted = true)
+                        @endif
 
-                </tbody>
-            </table>
-        </div>
+                        <tr>
+                            @foreach ($row as $column => $value)
+                                <td{!! (isset($headers[$column]['align-center']) && $headers[$column]['align-center']) ? ' class="text-center"' : null !!}>
+                                    {{ $value }}
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="box-body">No records found.</div>
+        @endif
     </div>
 @endsection
