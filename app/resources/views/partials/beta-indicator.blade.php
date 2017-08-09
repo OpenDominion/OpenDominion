03657a422613066c4c62da5a9c29e065db381c1e
WaveHack <email@wavehack.net>
2017-08-09 14:30:32 +0200

    Fix condition check

--- app/resources/views/partials/beta-indicator.blade.php
+++ app/resources/views/partials/beta-indicator.blade.php
@@ -1,4 +1,4 @@
-@if (Request::getHttpHost() !== 'dev.opendominion.wavehack.net')
+@if (Request::getHttpHost() === 'dev.opendominion.wavehack.net')
     <div style="padding: 10px 15px; background: rgb(243, 156, 18); z-index: 999999; font-size: 12px; font-weight: 400; color: #fff;">
         <i class="icon fa fa-warning"></i> This is the open beta of OpenDominion. As the game is still in development, not all features are present and some bugs may occur.
     </div>
