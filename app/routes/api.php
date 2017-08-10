797f015176ea737e05ee82e818ce6548203da0db
WaveHack <email@wavehack.net>
2017-08-10 21:04:10 +0200

    Add second pbbg test api

--- app/routes/api.php
+++ app/routes/api.php
@@ -14,4 +14,29 @@ $router->group(['prefix' => 'v1'], function (Router $router) {
         ];
     });
 
+    $router->get('pbbg2', function () {
+        return [
+            'name' => 'OpenDominion',
+            'version' => (Cache::has('version') ? Cache::get('version') : 'unknown'),
+            'description' => 'A text-based, persistent browser-based strategy game (PBBG) in a fantasy war setting',
+            'tags' => ['multiplayer', 'strategy', 'fantasy'],
+            'status' => 'up',
+            'dates' => [
+                'born' => '2013-02-04',
+                // 'updated' => '',
+            ],
+            'players' => [
+                'registered' => \OpenDominion\Models\User::count(),
+                'active' => \OpenDominion\Models\Dominion::whereHas('round', function ($q) {
+                    $q->where('start_date', '<=', \Carbon\Carbon::now())
+                        ->where('end_date', '>', \Carbon\Carbon::now());
+                })->count(),
+            ],
+            'links' => [
+                'beta' => 'https://dev.opendominion.wavehack.net/',
+                'github' => 'https://github.com/WaveHack/OpenDominion',
+            ],
+        ];
+    });
+
 });
