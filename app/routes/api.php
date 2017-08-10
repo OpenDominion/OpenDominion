443c327abf9dc27973d052c0ab0d52edebc603dd
WaveHack <email@wavehack.net>
2017-08-10 09:49:57 +0200

    Add API route for pbbg.site

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,1 +1,17 @@
 <?php
+
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+$router->group(['prefix' => 'v1'], function (Router $router) {
+
+    $router->get('pbbg', function () {
+        return [
+            'name' => 'OpenDominion',
+            'born' => '2013-02-04',
+            'registered_players' => \OpenDominion\Models\User::count(),
+            'tags' => ['multiplayer', 'strategy', 'fantasy']
+        ];
+    });
+
+});
