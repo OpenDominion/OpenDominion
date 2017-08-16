804b2600ee91b9084795ec1bfe11b3ad1abda272
WaveHack <email@wavehack.net>
2017-08-16 20:33:21 +0200

    Add development message to status protection box

--- app/resources/views/pages/dominion/status.blade.php
+++ app/resources/views/pages/dominion/status.blade.php
@@ -168,6 +168,8 @@
                     <div class="box-body">
                         <p>You are under a magical state of protection for <b>{{ number_format($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion), 2) }}</b> hours.</p>
                         <p>During protection you cannot be attacked or attack other dominions. You can neither cast any offensive spells or engage in espionage.</p>
+                        {{-- todo: remove line below once those things have been developed --}}
+                        <p><i>You can't do that regardless yet because OpenDominion is still in development and those features haven't been built yet.</i></p>
                         <p>You will leave protection at {{ $dominionProtectionService->getProtectionEndDate($selectedDominion)->format(DATE_RFC2822) }}.</p>
                         @if ($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion) > 71)
                             <p>No production occurs until you have less than 71 hours of protection remaining.</p>
