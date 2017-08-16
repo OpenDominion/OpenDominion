b7ce247fc9ce7c7497d5bc02cd1b60aace18ecd2
WaveHack <email@wavehack.net>
2017-08-16 18:49:11 +0200

    Add instant duration text thingy to bank page

--- app/resources/views/pages/dominion/bank.blade.php
+++ app/resources/views/pages/dominion/bank.blade.php
@@ -91,7 +91,7 @@
                     <h3 class="box-title">Information</h3>
                 </div>
                 <div class="box-body">
-                    <p>The National Bank allows you to exchange resources with the empire.</p>
+                    <p>The National Bank allows you to exchange resources with the empire. Exchanging resources is <b>instant</b>.</p>
                     <p>Platinum, lumber and ore trade 2 for 1.<br>Gems trade 1:2 platinum, lumber or ore.<br>Food sells for 4 platinum, lumber or ore, or 1 gem.</p>
                     <p>You have {{ number_format($selectedDominion->resource_gems) }} gems, {{ number_format($selectedDominion->resource_lumber) }} lumber, {{ number_format($selectedDominion->resource_ore) }} ore and {{ number_format($selectedDominion->resource_platinum) }} platinum.</p>
                 </div>
