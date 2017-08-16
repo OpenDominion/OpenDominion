c30774939d0976d2d1aba385ba444324b7b9fb38
WaveHack <email@wavehack.net>
2017-08-16 20:00:06 +0200

    Refactor status page

--- app/resources/views/pages/dominion/status.blade.php
+++ app/resources/views/pages/dominion/status.blade.php
@@ -3,172 +3,191 @@
 @section('page-header', 'Status')
 
 @section('content')
+    <div class="row">
 
-    @if ($dominionProtectionService->isUnderProtection($selectedDominion))
-        <div class="box box-warning">
-            <div class="box-body">
-                <p>You are under a magical state of protection for <b>{{ number_format($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion), 2) }}</b> hours.</p>
+        <div class="col-sm-12 col-md-9">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-bar-chart"></i> The Dominion of {{ $selectedDominion->name }}</h3>
+                </div>
+                <div class="box-body no-padding">
+                    <div class="row">
 
-                @if ($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion) > 71)
-                    <p>No production occurs until you have less than 71 hours of protection remaining.</p>
-                @endif
+                        <div class="col-xs-12 col-sm-4">
+                            <table class="table">
+                                <colgroup>
+                                    <col width="50%">
+                                    <col width="50%">
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Overview</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Ruler:</td>
+                                        <td>{{ Auth::user()->display_name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Race:</td>
+                                        <td>{{ $selectedDominion->race->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Land:</td>
+                                        <td>{{ number_format($landCalculator->getTotalLand($selectedDominion)) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Peasants:</td>
+                                        <td>{{ number_format($selectedDominion->peasants) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Employment:</td>
+                                        <td>{{ number_format($populationCalculator->getEmploymentPercentage($selectedDominion)) }}%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Networth:</td>
+                                        <td>{{ number_format($networthCalculator->getDominionNetworth($selectedDominion)) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Prestige:</td>
+                                        <td>{{ number_format($selectedDominion->prestige) }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-4">
+                            <table class="table">
+                                <colgroup>
+                                    <col width="50%">
+                                    <col width="50%">
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Resources</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Platinum:</td>
+                                        <td>{{ number_format($selectedDominion->resource_platinum) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Food:</td>
+                                        <td>{{ number_format($selectedDominion->resource_food) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lumber:</td>
+                                        <td>{{ number_format($selectedDominion->resource_lumber) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mana:</td>
+                                        <td>{{ number_format($selectedDominion->resource_mana) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ore:</td>
+                                        <td>{{ number_format($selectedDominion->resource_ore) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Gems:</td>
+                                        <td>{{ number_format($selectedDominion->resource_gems) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="nyi">Research Points:</td>
+                                        <td class="nyi">{{ number_format($selectedDominion->resource_tech) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Boats:</td>
+                                        <td>{{ number_format($selectedDominion->resource_boats) }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-4">
+                            <table class="table">
+                                <colgroup>
+                                    <col width="50%">
+                                    <col width="50%">
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Military</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Morale:</td>
+                                        <td>{{ number_format($selectedDominion->morale) }}%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Draftees:</td>
+                                        <td>{{ number_format($selectedDominion->military_draftees) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{{ $selectedDominion->race->units->get(0)->name }}:</td>
+                                        <td>{{ number_format($selectedDominion->military_unit1) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{{ $selectedDominion->race->units->get(1)->name }}:</td>
+                                        <td>{{ number_format($selectedDominion->military_unit2) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{{ $selectedDominion->race->units->get(2)->name }}:</td>
+                                        <td>{{ number_format($selectedDominion->military_unit3) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{{ $selectedDominion->race->units->get(3)->name }}:</td>
+                                        <td>{{ number_format($selectedDominion->military_unit4) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Spies:</td>
+                                        <td>{{ number_format($selectedDominion->military_spies) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Wizards:</td>
+                                        <td>{{ number_format($selectedDominion->military_wizards) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Archmages:</td>
+                                        <td>{{ number_format($selectedDominion->military_archmages) }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
-    @endif
 
-    <div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title"><i class="fa fa-bar-chart"></i> The Dominion of {{ $selectedDominion->name }}</h3>
-        </div>
-        <div class="box-body no-padding">
-            <div class="row">
-
-                <div class="col-xs-12 col-sm-4">
-                    <table class="table">
-                        <colgroup>
-                            <col width="50%">
-                            <col width="50%">
-                        </colgroup>
-                        <thead>
-                            <tr>
-                                <th colspan="2">Overview</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Ruler:</td>
-                                <td>{{ Auth::user()->display_name }}</td>
-                            </tr>
-                            <tr>
-                                <td>Race:</td>
-                                <td>{{ $selectedDominion->race->name }}</td>
-                            </tr>
-                            <tr>
-                                <td>Land:</td>
-                                <td>{{ number_format($landCalculator->getTotalLand($selectedDominion)) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Peasants:</td>
-                                <td>{{ number_format($selectedDominion->peasants) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Employment:</td>
-                                <td>{{ number_format($populationCalculator->getEmploymentPercentage($selectedDominion)) }}%</td>
-                            </tr>
-                            <tr>
-                                <td>Networth:</td>
-                                <td>{{ number_format($networthCalculator->getDominionNetworth($selectedDominion)) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Prestige:</td>
-                                <td>{{ number_format($selectedDominion->prestige) }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="col-sm-12 col-md-3">
+            @if ($dominionProtectionService->isUnderProtection($selectedDominion))
+                <div class="box box-warning">
+                    <div class="box-body">
+                        <p>You are under a magical state of protection for <b>{{ number_format($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion), 2) }}</b> hours.</p>
+                        <p>During protection you cannot be attacked or attack other dominions. You can neither cast any offensive spells or engage in espionage.</p>
+                        <p>You will leave protection at {{ $dominionProtectionService->getProtectionEndDate($selectedDominion)->format(DATE_RFC2822) }}.</p>
+                        @if ($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion) > 71)
+                            <p>No production occurs until you have less than 71 hours of protection remaining.</p>
+                        @endif
+                    </div>
                 </div>
+            @endif
 
-                <div class="col-xs-12 col-sm-4">
-                    <table class="table">
-                        <colgroup>
-                            <col width="50%">
-                            <col width="50%">
-                        </colgroup>
-                        <thead>
-                            <tr>
-                                <th colspan="2">Resources</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Platinum:</td>
-                                <td>{{ number_format($selectedDominion->resource_platinum) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Food:</td>
-                                <td>{{ number_format($selectedDominion->resource_food) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Lumber:</td>
-                                <td>{{ number_format($selectedDominion->resource_lumber) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Mana:</td>
-                                <td>{{ number_format($selectedDominion->resource_mana) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Ore:</td>
-                                <td>{{ number_format($selectedDominion->resource_ore) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Gems:</td>
-                                <td>{{ number_format($selectedDominion->resource_gems) }}</td>
-                            </tr>
-                            <tr>
-                                <td class="nyi">Research Points:</td>
-                                <td class="nyi">{{ number_format($selectedDominion->resource_tech) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Boats:</td>
-                                <td>{{ number_format($selectedDominion->resource_boats) }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+            {{-- todo: message about black ops not being enabled until 8th day in the round --}}
+
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Information</h3>
                 </div>
-
-                <div class="col-xs-12 col-sm-4">
-                    <table class="table">
-                        <colgroup>
-                            <col width="50%">
-                            <col width="50%">
-                        </colgroup>
-                        <thead>
-                            <tr>
-                                <th colspan="2">Military</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Morale:</td>
-                                <td>{{ number_format($selectedDominion->morale) }}%</td>
-                            </tr>
-                            <tr>
-                                <td>Draftees:</td>
-                                <td>{{ number_format($selectedDominion->military_draftees) }}</td>
-                            </tr>
-                            <tr>
-                                <td>{{ $selectedDominion->race->units->get(0)->name }}:</td>
-                                <td>{{ number_format($selectedDominion->military_unit1) }}</td>
-                            </tr>
-                            <tr>
-                                <td>{{ $selectedDominion->race->units->get(1)->name }}:</td>
-                                <td>{{ number_format($selectedDominion->military_unit2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>{{ $selectedDominion->race->units->get(2)->name }}:</td>
-                                <td>{{ number_format($selectedDominion->military_unit3) }}</td>
-                            </tr>
-                            <tr>
-                                <td>{{ $selectedDominion->race->units->get(3)->name }}:</td>
-                                <td>{{ number_format($selectedDominion->military_unit4) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Spies:</td>
-                                <td>{{ number_format($selectedDominion->military_spies) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Wizards:</td>
-                                <td>{{ number_format($selectedDominion->military_wizards) }}</td>
-                            </tr>
-                            <tr>
-                                <td>Archmages:</td>
-                                <td>{{ number_format($selectedDominion->military_archmages) }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="box-body">
+                    <p>This section gives you a quick overview of your dominion.</p>
+                    <p>Your total dominion networth is {{ number_format($networthCalculator->getDominionNetworth($selectedDominion)) }} and total land size is {{ number_format($landCalculator->getTotalLand($selectedDominion)) }}.</p>
                 </div>
-
             </div>
         </div>
+
     </div>
-
 @endsection
