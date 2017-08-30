8573b869c646da1a1218e8271ca8b81fe35f4e1a
WaveHack <email@wavehack.net>
2017-08-30 23:50:01 +0200

    Add activity event to user logged in event

--- src/Events/UserLoggedInEvent.php
+++ src/Events/UserLoggedInEvent.php
@@ -2,13 +2,23 @@
 
 namespace OpenDominion\Events;
 
+use OpenDominion\Contracts\Services\Activity\ActivityEvent as ActivityEventContract;
 use OpenDominion\Contracts\Services\Analytics\AnalyticsEvent as EventContract;
+use OpenDominion\Services\Activity\ActivityEvent;
+use OpenDominion\Services\Activity\HasActivityEvent;
 use OpenDominion\Services\Analytics\AnalyticsEvent;
 use OpenDominion\Services\Analytics\HasAnalyticsEvent;
 
-class UserLoggedInEvent extends AbstractUserEvent implements HasAnalyticsEvent
+class UserLoggedInEvent extends AbstractUserEvent implements HasActivityEvent, HasAnalyticsEvent
 {
-    // todo: activityevent
+    /**
+     * {@inheritdoc}
+     */
+    public function getActivityEvent(): ActivityEventContract
+    {
+        // todo: ioc
+        return new ActivityEvent('user.login', ActivityEventContract::STATUS_SUCCESS);
+    }
 
     /**
      * {@inheritdoc}
