fd843f07f53ff7e78f5ea8a88c19ea6d46ff27ca
WaveHack <email@wavehack.net>
2017-08-30 23:40:56 +0200

    Disable debug page for now

--- app/resources/views/partials/main-sidebar.blade.php
+++ app/resources/views/partials/main-sidebar.blade.php
@@ -42,10 +42,10 @@
 
                 {{--<li class="header">MISC</li>--}}
 
-                @if (app()->environment() !== 'production')
+                {{--@if (app()->environment() !== 'production')
                     <li class="header">SECRET</li>
                     <li class="{{ Request::is('dominion/debug') ? 'active' : null }}"><a href="{{ url('dominion/debug') }}"><i class="ra ra-dragon ra-fw"></i> <span>Debug Page</span></a></li>
-                @endif
+                @endif--}}
 
             @else
 
