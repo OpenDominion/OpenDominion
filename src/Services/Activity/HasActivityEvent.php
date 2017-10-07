f70adecf41c818c2bdec430cca1c4495a91aa243
WaveHack <email@wavehack.net>
2017-10-07 15:46:04 +0200

    Refactor HasActivityEvent

--- src/Services/Activity/HasActivityEvent.php
+++ src/Services/Activity/HasActivityEvent.php
@@ -2,7 +2,6 @@
 
 namespace OpenDominion\Services\Activity;
 
-use OpenDominion\Contracts\Services\Activity\ActivityEvent as ActivityEventContract;
 use OpenDominion\Models\User;
 
 interface HasActivityEvent
@@ -10,9 +9,9 @@ interface HasActivityEvent
     /**
      * Returns the ActivityEvent associated with this event.
      *
-     * @return ActivityEventContract
+     * @return ActivityEvent
      */
-    public function getActivityEvent(): ActivityEventContract;
+    public function getActivityEvent(): ActivityEvent;
 
     /**
      * Returns the User associated with this event.
