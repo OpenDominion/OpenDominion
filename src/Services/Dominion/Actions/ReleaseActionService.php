4befb251e8e0869ced20357f2a39680f15c75f29
pjonckiere <pieter.jonckiere@xio.be>
2017-10-15 12:08:58 +0200

    Fix an error in the calculation of the release success string

--- src/Services/Dominion/Actions/ReleaseActionService.php
+++ src/Services/Dominion/Actions/ReleaseActionService.php
@@ -81,17 +81,21 @@ class ReleaseActionService
             $troopsReleasedStringParts[] = sprintf('%s %s into the peasantry', number_format($amount), str_plural('draftee', $amount));
         }
 
+        $troopsCreated = FALSE;
         foreach ($troopsReleased as $unitType => $amount) {
-            if ($amount === 0 || $unitType === 'draftees') {
+            if ($unitType === 'draftees') {
                 continue;
             }
             else {
+                $troopsCreated = TRUE;
                 $conjunction = count($troopsReleasedStringParts) === 1 ? '' : 'and ';
                 $troopsReleasedStringParts[] = $conjunction . (number_format($amount) . ' ' . str_plural(str_singular(strtolower($this->unitHelper->getUnitName($unitType, $dominion->race))), $amount));
             }
         }
 
-        $troopsReleasedStringParts[] = 'into draftees';
+        if ($troopsCreated) {
+            $troopsReleasedStringParts[] = 'into draftees';
+        }
 
         $troopsReleasedString = (implode(' ', $troopsReleasedStringParts) . '.');
 
