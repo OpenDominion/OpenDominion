2dc25cd31d2459cb982895eda5768d77f54de2d5
WaveHack <email@wavehack.net>
2017-10-15 20:50:40 +0200

    Add the html for the favicons

--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -7,6 +7,15 @@
 
     <title>@yield('title', 'OpenDominion')</title>
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="OpenDominion">
+    <meta name="application-name" content="OpenDominion">
+    <meta name="theme-color" content="#ffffff">
+
     @include('partials.styles')
 
     <!--[if lt IE 9]>
