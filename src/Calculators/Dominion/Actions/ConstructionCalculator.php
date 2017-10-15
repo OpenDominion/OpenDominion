9034980a230b6088fb44251f43b67e66b27c24f2
WaveHack <email@wavehack.net>
2017-10-15 17:35:09 +0200

    Update formulae for platinum and lumber construction costs

--- src/Calculators/Dominion/Actions/ConstructionCalculator.php
+++ src/Calculators/Dominion/Actions/ConstructionCalculator.php
@@ -38,18 +38,27 @@ class ConstructionCalculator
         $totalBuildings = $this->buildingCalculator->getTotalBuildings($dominion);
         $totalLand = $this->landCalculator->getTotalLand($dominion);
 
-        if ($totalBuildings >= 1250) {
-            $platinum += max(
-                ($totalLand * 0.75),
-                $totalBuildings
-            );
-        } else {
-            $platinum += $totalLand;
-        }
+        // BlackReign's formula. Seems correct
+        $platinum += max(
+            (($totalBuildings < 250) ? 250 : $totalBuildings),
+            (3 * $totalBuildings) / 4
+        );
+
+        // Wiki formula. >= 1250 seems correct at start, but is reportedly incorrect later in the round
+//        if ($totalBuildings >= 1250) {
+//            $platinum += max(
+//                ($totalLand * 0.75),
+//                $totalBuildings
+//            );
+//        } else {
+//            $platinum += $totalLand;
+//        }
 
         $platinum -= 250;
         $platinum *= 1.53;
         $platinum += 850;
+
+        $platinum *= $this->getCostMultiplier($dominion);
 
         return (int)round($platinum);
     }
@@ -66,18 +75,29 @@ class ConstructionCalculator
         $totalBuildings = $this->buildingCalculator->getTotalBuildings($dominion);
         $totalLand = $this->landCalculator->getTotalLand($dominion);
 
-        if ($totalBuildings >= 1250) {
-            $lumber += max(
-                ($totalLand * 0.75),
-                $totalBuildings
-            );
-        } else {
-            $lumber += $totalLand;
-        }
+        // BlackReign's formula. Seems correct
+        $lumber += max(
+            (($totalBuildings < 250) ? 250 : $totalBuildings),
+            (3 * $totalBuildings) / 4
+        );
 
         $lumber -= 250;
-        $lumber *= 0.6;
-        $lumber += 88;
+        $lumber += 0.35;
+        $lumber += 87.5;
+
+        // Wiki formula. >= 1250 seems correct at start, but is reportedly incorrect later in the round
+//        if ($totalBuildings >= 1250) {
+//            $lumber += max(
+//                ($totalLand * 0.75),
+//                $totalBuildings
+//            );
+//        } else {
+//            $lumber += $totalLand;
+//        }
+//
+//        $lumber -= 250;
+//        $lumber *= 0.6;
+//        $lumber += 88;
 
         return (int)round($lumber);
     }
@@ -96,5 +116,42 @@ class ConstructionCalculator
             floor($dominion->resource_lumber / $this->getLumberCost($dominion)),
             $this->landCalculator->getTotalBarrenLand($dominion)
         );
+    }
+
+    public function getCostMultiplier(Dominion $dominion): float
+    {
+        $multiplier = 1.0;
+
+        // Values (percentages)
+        $factoryReduction = 4;
+        $factoryReductionMax = 75;
+
+        // Factories
+        $multiplier -= min(
+            (($dominion->building_factory / $this->landCalculator->getTotalLand($dominion)) * $factoryReduction),
+            ($factoryReductionMax / 100)
+        );
+
+        return $multiplier;
+
+        /*
+        todo: above formula should be good, but leaving this here this in case
+
+        buildings under construction = SUM(constructing home : constructing docks)
+
+        =IF(
+            landbonus? > 0;
+            IF(
+                buildings under construction > 0;
+                (
+                    (
+                        MIN(25%; (factories + factories destroy) / (land - landbonus? * 20))
+                        * (buildings under construction - landbonus? * 20)
+                ) + (factories + factories destroy) / (land) * landbonus? * 20) / buildings under construction;
+                (factories + factories destroy) / (land)
+            );
+            (factories + factories destroy) / land
+        )
+        */
     }
 }
