ace45b69672f82357b49f5d9275637ef395da7d4
WaveHack <email@wavehack.net>
2017-10-15 13:34:46 +0200

    Reorder inputs and use unsignedInteger() in migration

--- app/database/migrations/2017_10_15_112057_change_dominions_wizard_spy_strength_type.php
+++ app/database/migrations/2017_10_15_112057_change_dominions_wizard_spy_strength_type.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class ChangeDominionsWizardSpyStrengthType extends Migration
 {
@@ -27,8 +27,8 @@ class ChangeDominionsWizardSpyStrengthType extends Migration
     public function down()
     {
         Schema::table('dominions', function (Blueprint $table) {
-            $table->integer('spy_strength')->unsigned()->change();
-            $table->integer('wizard_strength')->unsigned()->change();
+            $table->unsignedInteger('spy_strength')->change();
+            $table->unsignedInteger('wizard_strength')->change();
         });
     }
 }
