7976ce4c0d5bf2c48bb6bbab22105b393b4b41d9
WaveHack <email@wavehack.net>
2017-10-15 19:27:47 +0200

    Cleanup construction calculator

--- src/Calculators/Dominion/Actions/ConstructionCalculator.php
+++ src/Calculators/Dominion/Actions/ConstructionCalculator.php
@@ -38,21 +38,10 @@ class ConstructionCalculator
         $totalBuildings = $this->buildingCalculator->getTotalBuildings($dominion);
         $totalLand = $this->landCalculator->getTotalLand($dominion);
 
-        // BlackReign's formula. Seems correct
         $platinum += max(
             (($totalBuildings < 250) ? 250 : $totalBuildings),
             (3 * $totalBuildings) / 4
         );
-
-        // Wiki formula. >= 1250 seems correct at start, but is reportedly incorrect later in the round
-//        if ($totalBuildings >= 1250) {
-//            $platinum += max(
-//                ($totalLand * 0.75),
-//                $totalBuildings
-//            );
-//        } else {
-//            $platinum += $totalLand;
-//        }
 
         $platinum -= 250;
         $platinum *= 1.53;
@@ -75,7 +64,6 @@ class ConstructionCalculator
         $totalBuildings = $this->buildingCalculator->getTotalBuildings($dominion);
         $totalLand = $this->landCalculator->getTotalLand($dominion);
 
-        // BlackReign's formula. Seems correct
         $lumber += max(
             (($totalBuildings < 250) ? 250 : $totalBuildings),
             (3 * $totalBuildings) / 4
@@ -84,20 +72,6 @@ class ConstructionCalculator
         $lumber -= 250;
         $lumber *= 0.35;
         $lumber += 87.5;
-
-        // Wiki formula. >= 1250 seems correct at start, but is reportedly incorrect later in the round
-//        if ($totalBuildings >= 1250) {
-//            $lumber += max(
-//                ($totalLand * 0.75),
-//                $totalBuildings
-//            );
-//        } else {
-//            $lumber += $totalLand;
-//        }
-//
-//        $lumber -= 250;
-//        $lumber *= 0.6;
-//        $lumber += 88;
 
         return (int)round($lumber);
     }
@@ -110,7 +84,6 @@ class ConstructionCalculator
      */
     public function getMaxAfford(Dominion $dominion): int
     {
-        // todo: factor in amount of barren land?
         return (int)min(
             floor($dominion->resource_platinum / $this->getPlatinumCost($dominion)),
             floor($dominion->resource_lumber / $this->getLumberCost($dominion)),
@@ -118,6 +91,12 @@ class ConstructionCalculator
         );
     }
 
+    /**
+     * Returns the Dominion's construction cost multiplier.
+     *
+     * @param Dominion $dominion
+     * @return float
+     */
     public function getCostMultiplier(Dominion $dominion): float
     {
         $multiplier = 1.0;
@@ -133,25 +112,5 @@ class ConstructionCalculator
         );
 
         return $multiplier;
-
-        /*
-        todo: above formula should be good, but leaving this here this in case
-
-        buildings under construction = SUM(constructing home : constructing docks)
-
-        =IF(
-            landbonus? > 0;
-            IF(
-                buildings under construction > 0;
-                (
-                    (
-                        MIN(25%; (factories + factories destroy) / (land - landbonus? * 20))
-                        * (buildings under construction - landbonus? * 20)
-                ) + (factories + factories destroy) / (land) * landbonus? * 20) / buildings under construction;
-                (factories + factories destroy) / (land)
-            );
-            (factories + factories destroy) / land
-        )
-        */
     }
 }
