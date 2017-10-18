517576bcae56f0d7efc4b0b9a6015a9c56165557
WaveHack <email@wavehack.net>
2017-10-18 23:33:47 +0200

    Add text about global plat prod mod not exceeding 50%

--- src/Helpers/ImprovementHelper.php
+++ src/Helpers/ImprovementHelper.php
@@ -33,7 +33,7 @@ class ImprovementHelper
     public function getImprovementHelpString(string $improvementType): string
     {
         $helpStrings = [
-            'science' => 'Improvements to science increase your platinum production.<br><br>Max +20% base science.',
+            'science' => 'Improvements to science increase your platinum production.<br><br>Max +20% base science. Global platinum production bonus cannot exceed +50%.',
             'keep' => 'Improvements to your keep increase your maximum population.<br><br>Max +30% base keep.',
             'towers' => 'Improvements to your towers increase your wizard strength and reduce damage from harmful spells.<br><br>Max +40% base towers.',
             'forges' => 'Improvements to your forges increase your offensive power.<br><br>Max +30% base forges.',
