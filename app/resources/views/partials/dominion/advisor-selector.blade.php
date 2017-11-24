170cea3ac934b6200f2d2301aa7c2154b8332086
WaveHack <email@wavehack.net>
2017-11-24 22:56:51 +0100

    Refactor advisor selector buttons, fixes #152

--- app/resources/views/partials/dominion/advisor-selector.blade.php
+++ app/resources/views/partials/dominion/advisor-selector.blade.php
@@ -2,59 +2,35 @@
     <div class="box-header with-border">
         <h3 class="box-title"><i class="fa fa-question-circle"></i> Consult advisor</h3>
     </div>
-    <div class="box-body">
-        <div class="row">
+    <div class="box-body text-center">
 
-            {{-- todo: refactor this --}}
+        <a href="{{ route('dominion.advisors.production') }}" class="btn btn-app">
+            <i class="fa fa-industry"></i> Production
+        </a>
 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.production') }}" class="btn btn-app btn-block">
-                    <i class="fa fa-industry"></i> Production
-                </a>
-            </div>
+        <a href="{{ route('dominion.advisors.military') }}" class="btn btn-app">
+            <i class="ra ra-sword"></i> Military
+        </a>
 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.military') }}" class="btn btn-app btn-block">
-                    <i class="ra ra-sword"></i> Military
-                </a>
-            </div>
+        <a href="{{ route('dominion.advisors.land') }}" class="btn btn-app">
+            <i class="ra ra-honeycomb"></i> Land
+        </a>
 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.land') }}" class="btn btn-app btn-block">
-                    <i class="ra ra-honeycomb"></i> Land
-                </a>
-            </div>
+        <a href="{{ route('dominion.advisors.construct') }}" class="btn btn-app">
+            <i class="fa fa-home"></i> Construction
+        </a>
 
-            <div class="col-xs-12 visible-xs">&nbsp;</div>
+        <a href="{{ route('dominion.advisors.magic') }}" class="btn btn-app">
+            <i class="ra ra-burning-embers"></i> Magic
+        </a>
 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.construct') }}" class="btn btn-app btn-block">
-                    <i class="fa fa-home"></i> Construction
-                </a>
-            </div>
+        <a href="{{ route('dominion.advisors.rankings', 'land') }}" class="btn btn-app">
+            <i class="fa fa-trophy"></i> Rankings
+        </a>
 
-            <div class="col-xs-12 visible-sm">&nbsp;</div>
+        <a href="{{ route('dominion.advisors.statistics') }}" class="btn btn-app">
+            <i class="fa fa-bar-chart"></i> Statistics
+        </a>
 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.magic') }}" class="btn btn-app btn-block">
-                    <i class="ra ra-burning-embers"></i> Magic
-                </a>
-            </div>
-
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.rankings', 'land') }}" class="btn btn-app btn-block">
-                    <i class="fa fa-trophy"></i> Rankings
-                </a>
-            </div>
-
-            <div class="col-xs-12 visible-xs visible-md">&nbsp;</div>
-
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                <a href="{{ route('dominion.advisors.statistics') }}" class="btn btn-app btn-block">
-                    <i class="fa fa-bar-chart"></i> Statistics
-                </a>
-            </div>
-
-        </div>
     </div>
 </div>
