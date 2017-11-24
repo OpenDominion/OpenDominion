c31905229c5323e23fd3ef51a6c47d143edcfaa2
WaveHack <email@wavehack.net>
2017-11-24 22:41:01 +0100

    Refactor improve action return string

--- src/Services/Dominion/Actions/ImproveActionService.php
+++ src/Services/Dominion/Actions/ImproveActionService.php
@@ -14,7 +14,7 @@ class ImproveActionService
     {
         $this->guardLockedDominion($dominion);
 
-        $data = array_map('intval', $data);
+        $data = array_map('\intval', $data);
 
         $totalResourcesToInvest = array_sum($data);
 
@@ -30,14 +30,7 @@ class ImproveActionService
             throw new RuntimeException("You do not have enough {$resource} to invest.");
         }
 
-        $worth = [
-            'platinum' => 1,
-            'lumber' => 2,
-            'ore' => 2,
-            'gems' => 12,
-        ];
-
-        $investmentStringParts = [];
+        $worth = $this->getImprovementWorth();
 
         foreach ($data as $improvementType => $amount) {
             if ($amount === 0) {
@@ -47,26 +40,64 @@ class ImproveActionService
             $points = ($amount * $worth[$resource]);
 
             $dominion->{'improvement_' . $improvementType} += $points;
-
-            $investmentStringParts[] = (number_format($points) . ' ' . $improvementType);
         }
 
-        $dominion->{'resource_' . $resource} -= $totalResourcesToInvest;
         $dominion->save();
 
-        $investmentString = generate_sentence_from_array($investmentStringParts);
-
         return [
-            'message' => sprintf(
-                'You invest %s %s into %s.',
-                number_format($totalResourcesToInvest),
-                ($resource === 'gems') ? str_plural('gem', $totalResourcesToInvest) : $resource,
-                $investmentString
-            ),
+            'message' => $this->getReturnMessageString($resource, $data, $totalResourcesToInvest),
             'data' => [
                 'totalResourcesInvested' => $totalResourcesToInvest,
                 'resourceInvested' => $resource,
             ],
+        ];
+    }
+
+    /**
+     * Returns the message for a improve action.
+     *
+     * @param string $resource
+     * @param array $data
+     * @param int $totalResourcesToInvest
+     * @return string
+     */
+    protected function getReturnMessageString(string $resource, array $data, int $totalResourcesToInvest): string
+    {
+        $worth = $this->getImprovementWorth();
+
+        $investmentStringParts = [];
+
+        foreach ($data as $improvementType => $amount) {
+            if ($amount === 0) {
+                continue;
+            }
+
+            $points = ($amount * $worth[$resource]);
+            $investmentStringParts[] = (number_format($points) . ' ' . $improvementType);
+        }
+
+        $investmentString = generate_sentence_from_array($investmentStringParts);
+
+        return sprintf(
+            'You invest %s %s into %s.',
+            number_format($totalResourcesToInvest),
+            ($resource === 'gems') ? str_plural('gem', $totalResourcesToInvest) : $resource,
+            $investmentString
+        );
+    }
+
+    /**
+     * Returns the amount of points per resource type invested.
+     *
+     * @return array
+     */
+    public function getImprovementWorth(): array
+    {
+        return [
+            'platinum' => 1,
+            'lumber' => 2,
+            'ore' => 2,
+            'gems' => 12,
         ];
     }
 }
