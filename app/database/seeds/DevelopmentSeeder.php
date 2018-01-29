499c6d9587c1e59c1bee6bc687a83b2ebaa7a93a
WaveHack <email@wavehack.net>
2018-01-29 14:17:56 +0100

    Create user roles, user, round, realm and dominion with DevelopmentSeeder instead of just round

--- app/database/seeds/DevelopmentSeeder.php
+++ app/database/seeds/DevelopmentSeeder.php
@@ -1,28 +1,88 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use OpenDominion\Models\Realm;
+use OpenDominion\Factories\DominionFactory;
+use OpenDominion\Models\Dominion;
+use OpenDominion\Models\Race;
 use OpenDominion\Models\Round;
 use OpenDominion\Models\RoundLeague;
 use OpenDominion\Models\User;
+use Spatie\Permission\Models\Role;
 
 class DevelopmentSeeder extends Seeder
 {
-    public function run()
+    /** @var DominionFactory */
+    protected $dominionFactory;
+
+    /**
+     * DevelopmentSeeder constructor.
+     *
+     * @param DominionFactory $dominionFactory
+     */
+    public function __construct(DominionFactory $dominionFactory)
     {
-        $this->createRound();
+        $this->dominionFactory = $dominionFactory;
     }
 
-    private function createRound()
+    public function run()
+    {
+        $this->createRoles();
+        $user = $this->createUser();
+        $round = $this->createRound();
+        $this->createRealmAndDominion($user, $round);
+    }
+
+    protected function createRoles()
+    {
+        $this->command->info('Creating user roles');
+
+        Role::create(['name' => 'Developer']);
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Moderator']);
+    }
+
+    protected function createUser(): User
+    {
+        $this->command->info('Creating test user');
+
+        $user = User::create([
+            'email' => 'email@example.com',
+            'password' => bcrypt('test'),
+            'display_name' => 'Dev User',
+            'activated' => true,
+            'activation_code' => str_random(),
+        ]);
+
+        $user->assignRole(['Developer', 'Administrator', 'Moderator']);
+
+        $this->command->info("User created. You may login with with email {$user->email} and password 'test'");
+
+        return $user;
+    }
+
+    protected function createRound(): Round
     {
         $this->command->info('Creating development round');
 
-        Round::create([
+        return Round::create([
             'round_league_id' => RoundLeague::where('key', 'standard')->firstOrFail()->id,
             'number' => 1,
-            'name' => 'Development Round',
+            'name' => 'Dev Round',
             'start_date' => new DateTime('today midnight'),
             'end_date' => new DateTime('+50 days midnight'),
         ]);
+    }
+
+    protected function createRealmAndDominion(User $user, Round $round): Dominion
+    {
+        $this->command->info('Creating realm and dominion');
+
+        return $this->dominionFactory->create(
+            $user,
+            $round,
+            Race::where('name', 'Human')->firstOrFail(),
+            'random',
+            'Dev Dominion'
+        );
     }
 }
