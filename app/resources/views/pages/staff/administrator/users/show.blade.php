3c85f4f702b8b1760ba0a97cb8bb899f5d7373e6
WaveHack <email@wavehack.net>
2018-02-02 01:29:54 +0100

    User show page

--- app/resources/views/pages/staff/administrator/users/show.blade.php
+++ app/resources/views/pages/staff/administrator/users/show.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.staff')
 
-@section('page-header', "User {$user->display_name}")
+@section('page-header', "User: {$user->display_name}")
 
 @section('content')
-    <p>user show</p>
-
     <pre>{{ print_r(json_decode($user), true) }}</pre>
 @endsection
