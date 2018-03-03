c0632f4d559b2f67c0ee8478f2a094314c748996
WaveHack <email@wavehack.net>
2018-03-03 23:18:59 +0100

    Refactor forgot password controller

--- src/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,12 +2,16 @@
 
 namespace OpenDominion\Http\Controllers\Auth;
 
+use Illuminate\Foundation\Auth\RedirectsUsers;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 use OpenDominion\Http\Controllers\AbstractController;
 
 class ForgotPasswordController extends AbstractController
 {
-    use SendsPasswordResetEmails;
+    use RedirectsUsers, SendsPasswordResetEmails;
+
+    protected $redirectTo = '/auth/password/reset';
 
     /**
      * {@inheritdoc}
@@ -15,5 +19,26 @@ class ForgotPasswordController extends AbstractController
     public function showLinkRequestForm()
     {
         return view('pages.auth.passwords.email');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function sendResetLinkResponse($response)
+    {
+        return redirect()
+            ->back()
+            ->with(
+                'alert-success',
+                'If that email address exists in our system, we will send it a reset password email.'
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return $this->sendResetLinkResponse($response);
     }
 }
