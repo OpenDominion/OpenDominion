c5d6358f9da8d72e6eaca0929090e2d94715f466
WaveHack <email@wavehack.net>
2018-03-04 20:17:36 +0100

    Add under protection-title to shield icons in realm page

--- app/resources/views/pages/dominion/realm.blade.php
+++ app/resources/views/pages/dominion/realm.blade.php
@@ -55,7 +55,7 @@
                                         <td class="text-center">{{ $i + 1 }}</td>
                                         <td>
                                             @if ($protectionService->isUnderProtection($dominion))
-                                                <i class="ra ra-shield ra-lg text-aqua"></i>
+                                                <i class="ra ra-shield ra-lg text-aqua" title="Under protection"></i>
                                             @endif
 
                                             {{--
