f7c9e959fc7c283826dd1f28ce291defc7829187
WaveHack <email@wavehack.net>
2018-03-10 16:35:49 +0100

    Remove unused view

--- app/resources/views/partials/settings-list-group.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="list-group">
-    {{-- todo: refactor. make array and loop through --}}
-    <a href="{{ route('settings.account') }}" class="list-group-item {{ Route::is('settings.account') ? 'active' : null }}">Account</a>
-    <a href="{{ route('settings.notifications') }}" class="list-group-item {{ Route::is('settings.notifications') ? 'active' : null }}">Notifications</a>
-    <a href="{{ route('settings.security') }}" class="list-group-item {{ Route::is('settings.security') ? 'active' : null }}">Security</a>
-</div>
