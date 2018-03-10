8fcbfa1518dfbe923b38f12705151a5b43900584
WaveHack <email@wavehack.net>
2018-03-10 16:29:04 +0100

    Add @auth on notification-nav

--- app/resources/views/partials/notification-nav.blade.php
+++ app/resources/views/partials/notification-nav.blade.php
@@ -1,18 +1,20 @@
-<li class="dropdown notifications-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-bell-o"></i>
-        <span class="label label-warning">10</span>
-    </a>
-    <ul class="dropdown-menu">
-        <li class="header">You have 10 notifications</li>
-        <li>
-            <ul class="menu">
-                <li>
-                    <a href="#">
-                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</li>
+@auth
+    <li class="dropdown notifications-menu">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-bell-o"></i>
+            <span class="label label-warning">10</span>
+        </a>
+        <ul class="dropdown-menu">
+            <li class="header">You have 10 notifications</li>
+            <li>
+                <ul class="menu">
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+@endauth
