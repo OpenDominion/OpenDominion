531abe37ce6b6e4a276e6933b8a7597d85844e29
WaveHack <email@wavehack.net>
2018-03-11 01:21:35 +0100

    Add back axios npm package

--- app/resources/assets/js/bootstrap.js
+++ app/resources/assets/js/bootstrap.js
@@ -1,3 +1,15 @@
 'use strict';
 
 window._ = require('lodash');
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
