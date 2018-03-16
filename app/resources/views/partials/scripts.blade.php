2aa9e65d4950d2c6875e2bae827b0ffc44a2f676
WaveHack <email@wavehack.net>
2018-03-16 01:41:00 +0100

    Fix style/script order

--- app/resources/views/partials/scripts.blade.php
+++ app/resources/views/partials/scripts.blade.php
@@ -1,13 +1,15 @@
 {{-- Vendor scripts --}}
 <script type="text/javascript" src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/vendor/admin-lte/js/adminlte.min.js') }}"></script>
-
-{{-- App scripts --}}
-<script type="text/javascript" src="{{ mix('assets/app/js/app.js') }}"></script>
 
 {{-- Page specific scripts --}}
 @stack('page-scripts')
 
 {{-- Page specific inline scripts --}}
 @stack('inline-scripts')
+
+{{-- AdminLTE scripts --}}
+<script type="text/javascript" src="{{ asset('assets/vendor/admin-lte/js/adminlte.min.js') }}"></script>
+
+{{-- App scripts --}}
+<script type="text/javascript" src="{{ mix('assets/app/js/app.js') }}"></script>
--- app/resources/views/partials/styles.blade.php
+++ app/resources/views/partials/styles.blade.php
@@ -2,17 +2,18 @@
 <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendor/font-awesome/css/font-awesome.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendor/rpg-awesome/css/rpg-awesome.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/vendor/admin-lte/css/AdminLTE.css') }}">
-
-<link rel="stylesheet" href="{{ asset('assets/vendor/admin-lte/css/skins/skin-blue.min.css') }}">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-{{-- App styles --}}
-<link rel="stylesheet" href="{{ mix('assets/app/css/app.css') }}">
 
 {{-- Page specific styles --}}
 @stack('page-styles')
 
 {{-- Page specific inline styles --}}
 @stack('inline-styles')
+
+{{-- AdminLTe styles --}}
+<link rel="stylesheet" href="{{ asset('assets/vendor/admin-lte/css/AdminLTE.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/admin-lte/css/skins/skin-blue.min.css') }}">
+
+{{-- App styles --}}
+<link rel="stylesheet" href="{{ mix('assets/app/css/app.css') }}">
