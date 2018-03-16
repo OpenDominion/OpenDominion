702a438de98cb6f6ac885636b5379b22486333c2
WaveHack <email@wavehack.net>
2018-03-16 01:41:19 +0100

    Working on magic page

--- app/resources/views/pages/dominion/magic.blade.php
+++ app/resources/views/pages/dominion/magic.blade.php
@@ -80,9 +80,42 @@
                         <div class="box-header with-border">
                             <h3 class="box-title"><i class="ra ra-burning-embers"></i> Offensive Spells</h3>
                         </div>
-                        <div class="box-body">
-                            <em>Not yet implemented</em>
-                        </div>
+                        <form action="{{ route('dominion.magic') }}" method="post" role="form">
+                            @csrf
+
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label for="target_dominion">Target</label>
+                                    <select name="target_dominion" id="target_dominion" class="form-control select2" style="width: 100%">
+                                        @foreach ($selectedDominion->round->realms()->with('dominions')->get() as $realm)
+                                            @if ($selectedDominion->realm->id == $realm->id)
+                                                @continue
+                                            @endif
+                                            @php
+                                            $dominions = $realm->dominions->sortByDesc(function ($dominion) use ($landCalculator) {
+                                                return $landCalculator->getTotalLand($dominion);
+                                            })->values();
+                                            @endphp
+                                            <optgroup label="{{ $realm->name }} (#{{ $realm->number }})">
+                                                @foreach ($dominions as $dominion)
+                                                    @php
+                                                    $land = $landCalculator->getTotalLand($dominion);
+                                                    @endphp
+                                                    <option value="{{ $dominion->id }}">
+                                                        {{ $dominion->name }}
+                                                        ({{ number_format($land) }} land)
+                                                    </option>
+                                                @endforeach
+                                            </optgroup>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                spells
+
+                                <em>Not yet implemented</em>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
@@ -105,3 +138,19 @@
 
     </div>
 @endsection
+
+@push('page-styles')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/select2/css/select2.min.css') }}">
+@endpush
+
+@push('page-scripts')
+    <script type="text/javascript" src="{{ asset('assets/vendor/select2/js/select2.full.min.js') }}"></script>
+@endpush
+
+@push('inline-scripts')
+    <script type="text/javascript">
+        (function ($) {
+            $('.select2').select2();
+        })(jQuery);
+    </script>
+@endpush
