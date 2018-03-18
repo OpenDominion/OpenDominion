65487e846f5f3f21ce35a3be7fea328438bb8272
WaveHack <email@wavehack.net>
2018-03-18 01:40:57 +0100

    Update mysql mode to strict

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
