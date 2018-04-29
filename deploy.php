556e369f6025d4f8dfce0c13cdee58f4d5a5afaf
WaveHack <email@wavehack.net>
2018-04-29 21:31:25 +0200

    Add Deployer file

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+set('repository', 'git@github.com:WaveHack/OpenDominion.git');
+set('branch', 'develop');
+
+set('ssh_multiplexing', false);
+
+host('opendominion.net')
+    ->set('deploy_path', '/var/www/opendominion.net')
+    ->configFile('~/.ssh/config');
+
+task('npm install', function () {
+    if (has('previous_release')) {
+        run('cp -R {{previous_release}}/node_modules {{release_path}}/node_modules');
+    }
+
+    run('cd {{release_path}} && npm install');
+});
+
+task('npm run prod', function () {
+    run('cd {{release_path}} && npm run prod');
+});
+
+after('deploy:vendors', 'npm install');
+after('npm install', 'npm run prod');
+
+before('deploy:symlink', 'artisan:migrate');
+
+before('artisan:migrate', 'artisan:down');
+after('deploy:symlink', 'artisan:up');
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy:failed', 'artisan:up');
