c967b9b24e5d2d856d6b4c91562e12aec9995d23
WaveHack <email@wavehack.net>
2018-04-30 18:31:58 +0200

    Reorganize deploy script

--- deploy.php
+++ deploy.php
@@ -4,15 +4,22 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
+// Config
+
 set('repository', 'git@github.com:WaveHack/OpenDominion.git');
 set('branch', 'develop');
 
 set('ssh_multiplexing', false);
 
+// Hosts
+
 host('opendominion.net')
     ->set('deploy_path', '/var/www/opendominion.net')
     ->configFile('~/.ssh/config');
 
+// Task definitions
+
+desc('Installing NPM dependencies');
 task('npm install', function () {
     if (has('previous_release')) {
         run('cp -R {{previous_release}}/node_modules {{release_path}}/node_modules');
@@ -21,23 +28,54 @@ task('npm install', function () {
     run('cd {{release_path}} && npm install');
 });
 
+desc('Build frontend');
 task('npm run prod', function () {
     run('cd {{release_path}} && npm run prod');
 });
 
+desc('Update version');
 task('artisan:version:update', function () {
-    run('{{bin/php}} {{release_path}}/artisan version:update');
+    run('cd {{release_path}} && {{bin/php}} artisan version:update');
 });
 
-after('deploy:vendors', 'npm install');
-after('npm install', 'npm run prod');
+desc('Reload php-fpm');
+task('php-fpm:reload', function () {
+    run('sudo service php7.2-fpm reload');
+});
 
-after('artisan:optimize', 'artisan:version:update');
+desc('Restart supervisor workers');
+task('supervisorctl:restart', function () {
+    run('sudo supervisorctl restart all');
+});
 
-before('deploy:symlink', 'artisan:migrate');
+// Execute tasks
 
-before('artisan:migrate', 'artisan:down');
-after('deploy:symlink', 'artisan:up');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'npm install', //
+    'npm run prod', //
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:clear',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    'artisan:optimize',
+    'artisan:down', //
+    'artisan:migrate', //
+    'artisan:version:update', //
+    'deploy:symlink',
+    'php-fpm:reload', //
+    'supervisorctl:restart', //
+    'artisan:up', //
+    'deploy:unlock',
+    'cleanup',
+]);
 
 after('deploy:failed', 'deploy:unlock');
 after('deploy:failed', 'artisan:up');
