dbf153e48abdfbe63fb888693cbd7b8a8d600c1a
WaveHack <email@wavehack.net>
2018-06-30 18:22:06 +0200

    Change Re-zone Land icon

--- app/resources/views/pages/dominion/rezone.blade.php
+++ app/resources/views/pages/dominion/rezone.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-12 col-md-9">
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title"><i class="ra ra-cycle"></i> Re-zone Land</h3>
+                    <h3 class="box-title"><i class="fa fa-refresh"></i> Re-zone Land</h3>
                 </div>
                 <form action="{{ route('dominion.rezone') }}" method="post" role="form">
                     @csrf
