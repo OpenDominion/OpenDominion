56c4c191f4b722cad5697aefdca728c3bb8c234b
WaveHack <email@wavehack.net>
2018-07-06 14:41:22 +0200

    Remove unused import

--- src/Console/Commands/Game/TickCommand.php
+++ src/Console/Commands/Game/TickCommand.php
@@ -5,7 +5,6 @@ namespace OpenDominion\Console\Commands\Game;
 use Illuminate\Console\Command;
 use OpenDominion\Console\Commands\CommandInterface;
 use OpenDominion\Services\Dominion\TickService;
-use Throwable;
 
 class TickCommand extends Command implements CommandInterface
 {
