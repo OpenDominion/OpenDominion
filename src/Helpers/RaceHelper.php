621f45e54c5bdc9aaf478155b89311f838528bbc
WaveHack <email@wavehack.net>
2018-07-30 23:05:19 +0200

    Reorder racial perks and add castle improvements for Goblin

--- src/Helpers/RaceHelper.php
+++ src/Helpers/RaceHelper.php
@@ -14,9 +14,9 @@ class RaceHelper
 
                 'nomad' => '<p>description here</p><p class="text-green">Increased mana production</p>',
 
-                'dwarf' => '<p>description here</p><p class="text-green">Increased ore production<br>Increased max population</p>',
+                'dwarf' => '<p>description here</p><p class="text-green">Increased max population<br>Increased ore production</p>',
 
-                'goblin' => '<p>description here</p><p class="text-green">Increased gem production<br>Increased max population</p>',
+                'goblin' => '<p>description here</p><p class="text-green">Increased max population<br>Increased gem production<br>Improved castle bonuses</p>',
 
             ][strtolower($race->name)] ?: 'todo';
     }
