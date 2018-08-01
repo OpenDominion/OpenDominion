3b8731354427026002eb98463799ff7b4c1780d0
WaveHack <email@wavehack.net>
2018-08-02 00:55:24 +0200

    Add dwarf racial description

--- src/Helpers/RaceHelper.php
+++ src/Helpers/RaceHelper.php
@@ -14,7 +14,7 @@ class RaceHelper
 
                 'nomad' => '<p>description here</p><p class="text-green">Increased mana production</p>',
 
-                'dwarf' => '<p>description here</p><p class="text-green">Increased max population<br>Increased ore production</p>',
+                'dwarf' => '<p>Short and grumpy little creatures.</p><p>Defined by their majestic beards and their love for booze and labor, these descendants of Caedair Hold have come to fight for the forces of good.</p><p>They have an intense hatred towards Goblins.</p><p class="text-green">Increased max population<br>Increased ore production</p>',
 
                 'goblin' => '<p>description here</p><p class="text-green">Increased max population<br>Increased gem production<br>Improved castle bonuses</p>',
 
