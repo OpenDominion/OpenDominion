ed4d46aeb101b579504f38c2b55589fd6f529e78
WaveHack <email@wavehack.net>
2018-08-26 00:00:46 +0200

    Add population, military and resource section for dominion nova resource

--- src/Nova/Dominion.php
+++ src/Nova/Dominion.php
@@ -5,7 +5,9 @@ namespace OpenDominion\Nova;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Panel;
 
 class Dominion extends Resource
 {
@@ -43,12 +45,6 @@ class Dominion extends Resource
         return [
             ID::make()->sortable(),
 
-            // user
-            // round
-            // realm
-            // race
-            // pack
-
             Text::make('Name')
                 ->sortable(),
 
@@ -61,10 +57,45 @@ class Dominion extends Resource
 
             BelongsTo::make('Round', 'round', Round::class),
 
-            // prestige
-            // peasants etc etc
+            // realm
+            // race
+            // pack
 
+            new Panel('Population', [
+                Number::make('Prestige'),
+                Number::make('Peasants')->hideFromIndex(),
+                Number::make('Peasants Last Hour')->hideFromIndex(),
+            ]),
 
+            new Panel('Military', [
+                Number::make('Draft Rate')->hideFromIndex(), // %
+                Number::make('Morale')->hideFromIndex(), // %
+                Number::make('Spy Strength')->hideFromIndex(), // %
+                Number::make('Wizard Strength')->hideFromIndex(), // %
+            ]),
+
+            new Panel('Resources', [
+                Number::make('Platinum', 'resource_platinum')->hideFromIndex(),
+                Number::make('Food', 'resource_food')->hideFromIndex(),
+                Number::make('Lumber', 'resource_lumber')->hideFromIndex(),
+                Number::make('Mana', 'resource_mana')->hideFromIndex(),
+                Number::make('Ore', 'resource_ore')->hideFromIndex(),
+                Number::make('Gems', 'resource_gems')->hideFromIndex(),
+                Number::make('Tech', 'resource_tech')->hideFromIndex(),
+                Number::make('Boats', 'resource_boats')->hideFromIndex()->step(0.01),
+            ]),
+
+            // improvements
+
+            // military
+
+            // land
+
+            // buildings
+
+            // #daily bonuses
+            // daily plat
+            // daily land
         ];
     }
 }
