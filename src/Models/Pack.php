f3198138c24ed34687d5306024cc711399d62b39
WaveHack <email@wavehack.net>
2018-10-05 22:27:34 +0200

    Add closed_at to packs and isFull/isClosed/getClosingDate methods

--- src/Models/Pack.php
+++ src/Models/Pack.php
@@ -2,10 +2,12 @@
 
 namespace OpenDominion\Models;
 
-use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class Pack extends AbstractModel
 {
+    protected $dates = ['closed_at', 'created_at', 'updated_at'];
+
     public function round()
     {
         return $this->belongsTo(Round::class);
@@ -24,5 +26,20 @@ class Pack extends AbstractModel
     public function realm()
     {
         return $this->belongsTo(Realm::class);
+    }
+
+    public function isFull(): bool
+    {
+        return ($this->dominions->count() === $this->size);
+    }
+
+    public function isClosed(): bool
+    {
+        return (($this->closed_at !== null) || $this->getClosingDate() < now());
+    }
+
+    public function getClosingDate(): Carbon
+    {
+        return max($this->created_at, $this->round->start_date)->addDays(3);
     }
 }
