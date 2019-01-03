ab7149619f2f9296daf2724b80149909d107492e
WaveHack <email@wavehack.net>
2019-01-03 22:58:02 +0100

    Fix honeypot field i

--- app/resources/views/vendor/honeypot/honeypotFormFields.blade.php
+++ app/resources/views/vendor/honeypot/honeypotFormFields.blade.php
@@ -1,6 +1,6 @@
-@if($enabled)
+@if ($enabled)
     <div id="{{ $nameFieldName }}_wrap" style="display:none;">
-        <input name="{{ $nameFieldName }}" type="text" value="" id="my_name">
+        <input name="{{ $nameFieldName }}" type="text" value="" id="poneyhot">
         <input name="{{ $validFromFieldName }}" type="text" value="{{ $encryptedValidFrom }}">
     </div>
 @endif
