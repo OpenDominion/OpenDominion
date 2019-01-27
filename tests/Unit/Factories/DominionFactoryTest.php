e7740f871ef7e966272f36a56edfbc15dd32417c
WaveHack <email@wavehack.net>
2019-01-27 12:23:03 +0100

    Fix test

--- tests/Unit/Factories/DominionFactoryTest.php
+++ tests/Unit/Factories/DominionFactoryTest.php
@@ -26,6 +26,9 @@ class DominionFactoryTest extends AbstractBrowserKitTestCase
     /** @var Race */
     protected $race;
 
+    /** @var Realm */
+    protected $realm;
+
     /** @var DominionFactory */
     protected $dominionFactory;
 
@@ -44,6 +47,7 @@ class DominionFactoryTest extends AbstractBrowserKitTestCase
         $this->user = $this->createUser();
         $this->round = $this->createRound();
         $this->race = Race::firstOrFail();
+        $this->realm = $this->createRealm($this->round, $this->race->alignment);
 
         $this->dominionFactory = $this->app->make(DominionFactory::class);
         $this->packService =  $this->app->make(PackService::class);
@@ -52,12 +56,16 @@ class DominionFactoryTest extends AbstractBrowserKitTestCase
 
     public function testCreate()
     {
-        $this->assertEquals(0, Realm::count());
         $this->assertEquals(0, Dominion::count());
 
-        $dominion = $this->dominionFactory->create($this->user, $this->round, $this->race, 'random', 'Ruler Name', 'Dominion Name', null);
+        $dominion = $this->dominionFactory->create(
+            $this->user,
+            $this->realm,
+            $this->race,
+            'Ruler Name',
+            'Dominion Name'
+        );
 
-        $this->assertEquals(1, Realm::count());
         $this->assertEquals(1, Dominion::count());
         $this->assertEquals($dominion->id, Dominion::first()->id);
     }
