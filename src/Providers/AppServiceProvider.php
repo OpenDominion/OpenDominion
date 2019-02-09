507342fa4057314b8ebb721f0937cd82e68af8d3
WaveHack <email@wavehack.net>
2019-02-09 16:36:56 +0100

    Remove Laravel Nova

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -72,11 +72,6 @@ class AppServiceProvider extends AbstractServiceProvider
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
 
-        /** @noinspection ClassConstantCanBeUsedInspection */
-        if (class_exists('Laravel\\Nova\\Nova')) {
-            $this->app->register(NovaServiceProvider::class);
-        }
-
         $this->registerCalculators();
         $this->registerServices();
     }
