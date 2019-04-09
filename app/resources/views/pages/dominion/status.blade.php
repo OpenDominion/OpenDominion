c85d6eb56331da03534cf57624fd3be2af75b423
WaveHack <email@wavehack.net>
2019-04-09 14:58:06 +0200

    Remove mention of invasions being NYI on status page protection info box

--- app/resources/views/pages/dominion/status.blade.php
+++ app/resources/views/pages/dominion/status.blade.php
@@ -192,8 +192,6 @@
                     <div class="box-body">
                         <p>You are under a magical state of protection for <b>{{ number_format($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion), 2) }}</b> {{ str_plural('hour', $dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion)) }}.</p>
                         <p>During protection you cannot be attacked or attack other dominions. You can neither cast any offensive spells or engage in espionage.</p>
-                        {{-- todo: remove line below once those things have been developed --}}
-                        <p><i>You can't do that regardless yet because OpenDominion is still in development and those features haven't been built yet.</i></p>
                         <p>You will leave protection on {{ $dominionProtectionService->getProtectionEndDate($selectedDominion)->format('l, jS \o\f F Y \a\t G:i') }}.</p>
                         @if ($dominionProtectionService->getUnderProtectionHoursLeft($selectedDominion) > 71)
                             <p>No production occurs until you have less than 71 hours of protection remaining.</p>
