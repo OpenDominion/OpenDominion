77f33314d44574a2b25cb860fbb43823338c4db6
WaveHack <email@wavehack.net>
2019-04-13 23:07:09 +0200

    Implement discounted construction costs for conquered aces, fixes #400

--- src/Services/Dominion/Actions/ConstructActionService.php
+++ src/Services/Dominion/Actions/ConstructActionService.php
@@ -82,8 +82,10 @@ class ConstructActionService
                 continue;
             }
 
-            $landType = $this->landHelper->getLandTypeForBuildingByRace(str_replace('building_', '', $buildingType),
-                $dominion->race);
+            $landType = $this->landHelper->getLandTypeForBuildingByRace(
+                str_replace('building_', '', $buildingType),
+                $dominion->race
+            );
 
             if (!isset($buildingsByLandType[$landType])) {
                 $buildingsByLandType[$landType] = 0;
@@ -99,26 +101,52 @@ class ConstructActionService
         }
 
         $platinumCost = ($this->constructionCalculator->getPlatinumCost($dominion) * $totalBuildingsToConstruct);
-        $newPlatinum = ($dominion->resource_platinum - $platinumCost);
-
         $lumberCost = ($this->constructionCalculator->getLumberCost($dominion) * $totalBuildingsToConstruct);
+
+        // Check for discounted acres after invasion
+        $discountedBuildings = min($dominion->discounted_land, $totalBuildingsToConstruct);
+        $platinumDiscount = 0;
+        $lumberDiscount = 0;
+
+        if ($discountedBuildings > 0) {
+            // Calculate half cost
+            $platinumDiscount = (int)ceil(($this->constructionCalculator->getPlatinumCost($dominion) * $discountedBuildings) / 2); // todo: constant this with -50% or something
+            $lumberDiscount = (int)ceil(($this->constructionCalculator->getLumberCost($dominion) * $discountedBuildings) / 2);
+
+            $platinumCost -= $platinumDiscount;
+            $lumberCost -= $lumberDiscount;
+        }
+
+        $newPlatinum = ($dominion->resource_platinum - $platinumCost);
         $newLumber = ($dominion->resource_lumber - $lumberCost);
 
-        DB::transaction(function () use ($dominion, $data, $newPlatinum, $newLumber) {
+        DB::transaction(function () use ($dominion, $data, $newPlatinum, $newLumber, $discountedBuildings) {
             $dominion->fill([
                 'resource_platinum' => $newPlatinum,
                 'resource_lumber' => $newLumber,
+                'discounted_land' => ($dominion->discounted_land - $discountedBuildings),
             ])->save(['event' => HistoryService::EVENT_ACTION_CONSTRUCT]);
 
             $this->queueService->queueResources('construction', $dominion, $data);
         });
 
+        $message = sprintf(
+            'Construction started at a cost of %s platinum and %s lumber.',
+            number_format($platinumCost),
+            number_format($lumberCost)
+        );
+
+        if ($discountedBuildings > 0) {
+            $message .= sprintf(
+                ' Because of %s acres of conquered land, you saved %s platinum and %s lumber.',
+                number_format($discountedBuildings),
+                number_format($platinumDiscount),
+                number_format($lumberDiscount)
+            );
+        }
+
         return [
-            'message' => sprintf(
-                'Construction started at a cost of %s platinum and %s lumber.',
-                number_format($platinumCost),
-                number_format($lumberCost)
-            ),
+            'message' => $message,
             'data' => [
                 'platinumCost' => $platinumCost,
                 'lumberCost' => $lumberCost,
