3d2ed27bed0153ee9d383f7f335201bc47662e90
John Strömblom <john.stromblom@outlook.com>
2019-04-16 00:23:24 +0200

    Moved to military calc for military pop calculation.

--- src/Calculators/Dominion/PopulationCalculator.php
+++ src/Calculators/Dominion/PopulationCalculator.php
@@ -18,6 +18,9 @@ class PopulationCalculator
     /** @var LandCalculator */
     protected $landCalculator;
 
+    /** @var MilitaryCalculator */
+    protected $militaryCalculator;
+
     /** @var QueueService */
     protected $queueService;
 
@@ -41,6 +44,7 @@ class PopulationCalculator
         BuildingHelper $buildingHelper,
         ImprovementCalculator $improvementCalculator,
         LandCalculator $landCalculator,
+        MilitaryCalculator $militaryCalculator,
         QueueService $queueService,
         SpellCalculator $spellCalculator,
         UnitHelper $unitHelper
@@ -48,6 +52,7 @@ class PopulationCalculator
         $this->buildingHelper = $buildingHelper;
         $this->improvementCalculator = $improvementCalculator;
         $this->landCalculator = $landCalculator;
+        $this->militaryCalculator = $militaryCalculator;
         $this->queueService = $queueService;
         $this->spellCalculator = $spellCalculator;
         $this->unitHelper = $unitHelper;
@@ -77,17 +82,14 @@ class PopulationCalculator
     {
         return (
             $dominion->military_draftees
-            + $dominion->military_unit1
-            + $dominion->military_unit2
-            + $dominion->military_unit3
-            + $dominion->military_unit4
+            + $this->militaryCalculator->getTotalUnitsForSlot($dominion, 1)
+            + $this->militaryCalculator->getTotalUnitsForSlot($dominion, 2)
+            + $this->militaryCalculator->getTotalUnitsForSlot($dominion, 3)
+            + $this->militaryCalculator->getTotalUnitsForSlot($dominion, 4)
             + $dominion->military_spies
             + $dominion->military_wizards
             + $dominion->military_archmages
             + $this->queueService->getTrainingQueueTotal($dominion)
-            + $this->queueService->getInvasionQueueTotalByResource($dominion, "military_unit1")
-            + $this->queueService->getInvasionQueueTotalByResource($dominion, "military_unit3")
-            + $this->queueService->getInvasionQueueTotalByResource($dominion, "military_unit4")
         );
     }
 
