32ab08e0ccc6b77a1d93785e39f748ae14287964
John Strömblom <john.stromblom@outlook.com>
2019-06-01 01:03:04 +0200

    Refactored TownCrierController, moved the code into a new service. Reused it for clarivoyance.

---
+++ src/Services/GameEventService.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace OpenDominion\Services;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use OpenDominion\Models\Dominion;
+use OpenDominion\Models\GameEvent;
+use OpenDominion\Models\Realm;
+
+class GameEventService
+{
+    public function getTownCrier(Dominion $dominion) : array
+    {
+        $realm = $dominion->realm;
+
+        return $this->getGameEventsForRealm($realm, now());
+    }
+
+    public function getClairvoyance(int $realmId, Carbon $clairvoyanceUpdateAt)
+    {
+        $realm = Realm::find($realmId);
+        return $this->getGameEventsForRealm($realm, $clairvoyanceUpdateAt);
+    }
+
+    private function getGameEventsForRealm(Realm $realm, Carbon $createdBefore) : array
+    {
+        $dominionIds = $realm->dominions
+            ->pluck('id')
+            ->toArray();
+
+        $gameEvents = GameEvent::query()
+            ->where('round_id', $realm->round->id)
+            ->where('created_at', '<', $createdBefore)
+            ->where('created_at', '>', now()->subDays(7))
+            ->where(function (Builder $query) use ($realm, $dominionIds) {
+                $query
+                    ->orWhere(function (Builder $query) use ($dominionIds) {
+                        $query->where('source_type', Dominion::class)
+                            ->whereIn('source_id', $dominionIds);
+                    })
+                    ->orWhere(function (Builder $query) use ($dominionIds) {
+                        $query->where('target_type', Dominion::class)
+                            ->whereIn('target_id', $dominionIds);
+                    })
+                    ->orWhere(function (Builder $query) use ($realm) {
+                        $query->where('source_type', Realm::class)
+                            ->where('source_id', $realm->id);
+                    })
+                    ->orWhere(function (Builder $query) use ($realm) {
+                        $query->where('target_type', Realm::class)
+                            ->where('target_id', $realm->id);
+                    });
+            })
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return [
+            "dominionIds" => $dominionIds,
+            "gameEvents" =>  $gameEvents
+        ];
+    }
+}
