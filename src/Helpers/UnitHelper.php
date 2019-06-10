90e2e89abba2c5f9017e2f24074e3a1682b87408
internetfett <internetfett@gmail.com>
2019-06-10 00:46:44 -0500

    additional unit perk descriptions

--- src/Helpers/UnitHelper.php
+++ src/Helpers/UnitHelper.php
@@ -61,14 +61,52 @@ class UnitHelper
             'fewer_casualties_offense' => '%s%% fewer casualties on offense.',
             'counts_as_wizard_offense' => 'Each unit counts as %s of a wizard on offense.',
             'counts_as_wizard_defense' => 'Each unit counts as %s of a wizard on defense.',
-            'immortal_vs_land_range' => 'Immortal when attacking %s%% of your size or greater.',
+            'immortal_vs_land_range' => 'Immortal when attacking dominions %s%% of your size.',
             'counts_as_spy_offense' => 'Each unit counts as %s of a spy on offense.',
             'counts_as_spy_defense' => 'Each unit counts as %s of a spy on defense.',
+            'fixed_casualties' => 'ALWAYS suffers %s%% casualties.',
+            'offense_from_building' => 'Offense increased by 1 for every %2$s%% %1$ss (max +%3$s).',
+            'defense_from_building' => 'Defense increased by 1 for every %2$s%% %1$ss (max +%3$s).',
+            'offense_from_land' => 'Offense increased by 1 for every %2$s%% %1$ss (max +%3$s).',
+            'defense_from_land' => 'Defense increased by 1 for every %2$s%% %1$ss (max +%3$s).',
+            'offense_raw_wizard_ratio' => 'Offense increased by %1$s * Raw Wizard Ratio (max +%2$s).',
+            'offense_staggered_land_range' => 'Offense increased by %2$s against dominions %1$s%% of your size.',
+            'offense_vs_goblin' => 'Offense increased by %s against goblins.',
+            'defense_vs_goblin' => 'Defense increased by %s against goblins.',
         ];
 
         foreach ($race->units as $unit) {
             foreach ($unit->perks->whereIn('key', array_keys($perkTypeStrings)) as $perk) {
-                $helpStrings['unit' . $unit->slot] .= ('<br><br>' . sprintf($perkTypeStrings[$perk->key], $perk->pivot->value));
+                $perkValue = $perk->pivot->value;
+
+                // Handle array-based perks
+                $nestedArrays = false;
+                // todo: refactor all of this
+                // partially copied from Race::getUnitPerkValueForUnitSlot
+                if (str_contains($perkValue, ',')) {
+                    $perkValue = explode(',', $perkValue);
+
+                    foreach ($perkValue as $key => $value) {
+                        if (!str_contains($value, ';')) {
+                            continue;
+                        }
+
+                        $nestedArrays = true;
+                        $perkValue[$key] = explode(';', $value);
+                    }
+                }
+
+                if (is_array($perkValue)) {
+                    if ($nestedArrays) {
+                        foreach ($perkValue as $nestedKey => $nestedValue) {
+                            $helpStrings['unit' . $unit->slot] .= ('<br><br>' . vsprintf($perkTypeStrings[$perk->key], $nestedValue));
+                        }
+                    } else {
+                        $helpStrings['unit' . $unit->slot] .= ('<br><br>' . vsprintf($perkTypeStrings[$perk->key], $perkValue));
+                    }
+                } else {
+                    $helpStrings['unit' . $unit->slot] .= ('<br><br>' . sprintf($perkTypeStrings[$perk->key], $perkValue));
+                }
             }
         }
 
