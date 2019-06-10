e3ce1062d9b9a0925dd5416e8a5d15865ab46585
WaveHack <email@wavehack.net>
2019-06-10 02:31:29 +0200

    Add link to My First Round wiki article on home page

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -112,6 +112,7 @@
                     <p>To help you get started, please consult the following resources:</p>
 
                     <ul>
+                        <li><a href="https://opendominion.miraheze.org/wiki/My_First_Round" target="_blank">My First Round <i class="fa fa-external-link"></i></a> on the <a href="https://opendominion.miraheze.org/" target="_blank">OpenDominion Wiki <i class="fa fa-external-link"></i></a>.</li>
                         <li>A mirror of Dominion's manual: <a href="https://dominion.opendominion.net/scribes.html" target="_blank">The Scribes <i class="fa fa-external-link"></i></a></li>
                         <li><a href="http://web.archive.org/web/20131226013425/http://dominion.lykanthropos.com:80/wiki/index.php/The_Complete_Newbie_Guide" target="_blank">The Complete Newbie Guide <i class="fa fa-external-link"></i></a> on the Web Archive</li>
                     </ul>
