f03111e4cf73a5f3c347db7e627e266c44ca04d1
WaveHack <email@wavehack.net>
2019-06-12 01:10:26 +0200

    Add ManualEmailNotification class

---
+++ src/Notifications/ManualEmailNotification.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace OpenDominion\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use OpenDominion\Models\User;
+
+class ManualEmailNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /** @var string */
+    protected $subject;
+
+    /** @var string */
+    protected $greeting;
+
+    /** @var array */
+    protected $lines;
+
+    /** @var array */
+    protected $action;
+
+    /**
+     * ManualEmailNotification constructor.
+     *
+     * @param string $subject
+     * @param string $greeting
+     * @param array $lines
+     * @param array $action [Label => URL]
+     */
+    public function __construct(string $subject, string $greeting, array $lines, array $action)
+    {
+        $this->subject = $subject;
+        $this->greeting = $greeting;
+        $this->lines = $lines;
+        $this->action = $action;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param User $user
+     * @return array
+     */
+    public function via(User $user): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param User $user
+     * @return MailMessage
+     */
+    public function toMail(User $user): MailMessage
+    {
+        $mailMessage = (new MailMessage)
+            ->replyTo('email@wavehack.net', 'WaveHack')
+            ->subject($this->subject)
+            ->greeting($this->greeting);
+
+        foreach ($this->lines as $line) {
+            $mailMessage = $mailMessage->line($line);
+        }
+
+        $mailMessage = $mailMessage->line('You are receiving this email because you have turned on email notifications for generic emails manually sent by the administrators.')
+            ->line('To unsubscribe, please update your notification settings at: ' . route('settings'))
+            ->salutation('-OpenDominion');
+
+        return $mailMessage;
+    }
+}
