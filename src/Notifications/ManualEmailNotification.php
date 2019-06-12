9ca6a97038634834e803ec5f13120f963b6d6c09
WaveHack <email@wavehack.net>
2019-06-12 14:48:55 +0200

    Implement action button in manual email notification

--- src/Notifications/ManualEmailNotification.php
+++ src/Notifications/ManualEmailNotification.php
@@ -30,9 +30,9 @@ class ManualEmailNotification extends Notification implements ShouldQueue
      * @param string $subject
      * @param string $greeting
      * @param array $lines
-     * @param array $action [Label => URL]
+     * @param array|null $action [Label => URL]
      */
-    public function __construct(string $subject, string $greeting, array $lines, array $action)
+    public function __construct(string $subject, string $greeting, array $lines, ?array $action = null)
     {
         $this->subject = $subject;
         $this->greeting = $greeting;
@@ -66,6 +66,12 @@ class ManualEmailNotification extends Notification implements ShouldQueue
 
         foreach ($this->lines as $line) {
             $mailMessage = $mailMessage->line($line);
+        }
+
+        if ($this->action !== null) {
+            foreach ($this->action as $label => $url) {
+                $mailMessage = $mailMessage->action($label, $url);
+            }
         }
 
         $mailMessage = $mailMessage->line('You are receiving this email because you have turned on email notifications for generic emails manually sent by the administrators.')
