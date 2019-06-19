95a2a59215cc839ab9156155b43af36c51838290
John Strömblom <john.stromblom@outlook.com>
2019-06-20 01:44:46 +0200

    Added use of getUnitPowerWithPerk from military calc in nw calc.

--- src/Calculators/NetworthCalculator.php
+++ src/Calculators/NetworthCalculator.php
@@ -75,7 +75,7 @@ class NetworthCalculator
 
         foreach ($dominion->race->units as $unit) {
             $totalUnitsOfType = $this->militaryCalculator->getTotalUnitsForSlot($dominion, $unit->slot);
-            $networth += $totalUnitsOfType * $this->getUnitNetworth($unit);
+            $networth += $totalUnitsOfType * $this->getUnitNetworth($unit, $dominion);
         }
 
         $networth += ($dominion->military_spies * $networthPerSpy);
@@ -94,19 +94,23 @@ class NetworthCalculator
     /**
      * Returns a single Unit's networth.
      *
+     * @param Dominion $dominion
      * @param Unit $unit
      * @return float
      */
-    public function getUnitNetworth(Unit $unit): float
+    public function getUnitNetworth(Dominion $dominion, Unit $unit): float
     {
         if (in_array($unit->slot, [1, 2], false)) {
             return 5;
         }
 
+        $unitOffense = $this->militaryCalculator->getUnitPowerWithPerks($dominion, null, 1, $unit, "offense");
+        $unitDefense = $this->militaryCalculator->getUnitPowerWithPerks($dominion, null, 1, $unit, "defense");
+
         return (
-            (1.8 * min(6, max($unit->power_offense, $unit->power_defense)))
-            + (0.45 * min(6, min($unit->power_offense, $unit->power_defense)))
-            + (0.2 * (max(($unit->power_offense - 6), 0) + max(($unit->power_defense - 6), 0)))
+            (1.8 * min(6, max($unitOffense, $unitDefense)))
+            + (0.45 * min(6, min($unitOffense, $unitDefense)))
+            + (0.2 * (max(($unitOffense - 6), 0) + max(($unitDefense - 6), 0)))
         );
     }
 }
