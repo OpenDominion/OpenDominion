4153678c0f11fca5d3fe5ab0f8d947a52b8b7936
WaveHack <email@wavehack.net>
2019-07-14 01:33:32 +0200

    Refactor JavaScript

---
+++ app/resources/js/helpers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Formats a certain amount of bytes into a human-readable string.
+ *
+ * @param {number} bytes
+ * @param {number} decimals
+ * @returns {string}
+ */
+function formatBytes(bytes, decimals) {
+    if (bytes === 0) {
+        return '0 Bytes';
+    }
+
+    const k = 1024,
+        dm = (decimals || 2),
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+module.exports = {
+    formatBytes,
+};
--- app/resources/js/ticker.js
+++ app/resources/js/ticker.js
@@ -1,64 +1,103 @@
+'use strict';
 
-// todo: there's probably a more elegant way to implement this :)
+class Ticker {
+    /**
+     * Ticker constructor.
+     */
+    constructor() {
+        this.tickerServerElement = null;
+        this.tickerNextHourElement = null;
+    }
 
-let currentDateNow = null;
+    /**
+     * Starts the ticker.
+     */
+    start() {
+        this.tickerServerElement = document.getElementById('ticker-server');
+        this.tickerNextHourElement = document.getElementById('ticker-next-tick');
 
-function ticker() {
-    setInterval(tick, 1000);
-}
+        const self = this;
+        setInterval(() => self.tick(), 1000);
+    }
 
-function tick() {
-    const el = document.getElementById('tickers');
+    /**
+     * Ticks the ticker.
+     *
+     * @private
+     */
+    tick() {
+        const currentTime = new Date;
 
-    if (el != null) {
-        if(currentDateNow == null)
-        {
-            const currentServerTime = el.getElementsByClassName('ticker-server')[0].innerHTML;
-            currentDateNow = new Date('1970-01-01T' + currentServerTime + 'Z');
-        }
-
-        currentDateNow.setUTCSeconds(currentDateNow.getUTCSeconds() + 1);
-
-        const nextHour = new Date(currentDateNow);
-        nextHour.setUTCHours(currentDateNow.getUTCHours() + 1);
+        const nextHour = new Date;
+        nextHour.setUTCHours(currentTime.getUTCHours() + 1);
         nextHour.setMinutes(0);
         nextHour.setSeconds(0);
 
-        el.getElementsByClassName('ticker-server')[0].innerHTML = hms(utc(currentDateNow));
-        el.getElementsByClassName('ticker-next-tick')[0].innerHTML = hms(nextHour - currentDateNow);
+        const diffDate = (nextHour - currentTime);
+
+        this.tickerServerElement.innerHTML = Ticker.hms(Ticker.utc(currentTime));
+        this.tickerNextHourElement.innerHTML = Ticker.hms(diffDate);
+    }
+
+    /**
+     * Formats a Date or numeric value.
+     *
+     * @param {Date|number} value
+     * @return {string}
+     * @private
+     */
+    static hms(value) {
+        let hours = 0;
+        let minutes = 0;
+        let seconds = 0;
+
+        if (value instanceof Date) {
+            hours = value.getHours();
+            minutes = value.getMinutes();
+            seconds = value.getSeconds();
+
+        } else if (typeof value === 'number') {
+            value /= 1000;
+            seconds = value % 60;
+            minutes = (value - seconds) / 60;
+            hours = (value - (minutes * 60) - seconds) / 60;
+        }
+
+        return (
+            Ticker.pad(hours) +
+            ':' + Ticker.pad(minutes) +
+            ':' + Ticker.pad(seconds)
+        );
+    }
+
+    /**
+     * Left-pads a numeric value with two zeroes.
+     *
+     * @param {number} value
+     * @returns {string}
+     * @private
+     */
+    static pad(value) {
+        return ('00' + value).slice(-2);
+    }
+
+    /**
+     * Converts a Date into UTC.
+     *
+     * @param {Date} date
+     * @returns {Date}
+     * @private
+     */
+    static utc(date) {
+        return new Date(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate(),
+            date.getUTCHours(),
+            date.getUTCMinutes(),
+            date.getUTCSeconds()
+        );
     }
 }
 
-function hms(value) {
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    if (value instanceof Date) {
-        hours = value.getHours();
-        minutes = value.getMinutes();
-        seconds = value.getSeconds();
-    } else if (typeof value == 'number') {
-        value = parseInt(value) / 1000;
-        seconds = value % 60;
-        minutes = (value - seconds) / 60;
-        hours = (value - (minutes * 60) - seconds) / 60;
-    }
-    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
-}
-
-function pad(value) {
-    return ('00' + value).slice(-2);
-}
-
-function utc(date) {
-    return new Date(
-        date.getUTCFullYear(),
-        date.getUTCMonth(),
-        date.getUTCDate(),
-        date.getUTCHours(),
-        date.getUTCMinutes(),
-        date.getUTCSeconds()
-    )
-}
-
-module.exports = ticker;
+module.exports = new Ticker;
