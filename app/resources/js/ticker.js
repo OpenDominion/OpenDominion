94c869d87b9b991120b69b1d1d72fc4abd1c2187
WaveHack <email@wavehack.net>
2019-07-24 13:46:39 +0200

    Fix typecast in ticker

--- app/resources/js/ticker.js
+++ app/resources/js/ticker.js
@@ -33,7 +33,7 @@ class Ticker {
         const currentTime = new Date('1970-01-01T' + currentServerTime + 'Z');
         currentTime.setUTCSeconds(currentTime.getUTCSeconds() + 1);
 
-        const nextHour = new Date(currentTime);
+        const nextHour = new Date(currentTime.toString());
         nextHour.setUTCHours(currentTime.getUTCHours() + 1);
         nextHour.setMinutes(0);
         nextHour.setSeconds(0);
