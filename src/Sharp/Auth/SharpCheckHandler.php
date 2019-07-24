01be95ee2aa6cc99cbe22b7b417c7119cbe23b79
WaveHack <email@wavehack.net>
2019-07-24 14:44:28 +0200

    Change Sharp check handler

--- src/Sharp/Auth/SharpCheckHandler.php
+++ src/Sharp/Auth/SharpCheckHandler.php
@@ -3,18 +3,16 @@
 namespace OpenDominion\Sharp\Auth;
 
 use Code16\Sharp\Auth\SharpAuthenticationCheckHandler;
-use Illuminate\Contracts\Auth\Authenticatable;
+use OpenDominion\Models\User;
 
 class SharpCheckHandler implements SharpAuthenticationCheckHandler
 {
     /**
-     * @param Authenticatable $user
+     * @param User $user
      * @return bool
      */
     public function check($user): bool
     {
-        return in_array($user->email, [
-            base64_decode('ZW1haWxAd2F2ZWhhY2submV0'), // WaveHack
-        ], true);
+        return $user->isStaff();
     }
 }
