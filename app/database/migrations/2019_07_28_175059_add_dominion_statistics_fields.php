396e27f7859e7a46468dd226305987c7f2625c51
WaveHack <email@wavehack.net>
2019-07-28 19:59:21 +0200

    Add migration for dominion statistics

---
+++ app/database/migrations/2019_07_28_175059_add_dominion_statistics_fields.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDominionStatisticsFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('dominions', static function (Blueprint $table) {
+            $table->unsignedInteger('stat_attacking_success')->after('building_dock')->default(0);
+            $table->unsignedInteger('stat_defending_success')->after('stat_attacking_success')->default(0);
+            $table->unsignedInteger('stat_espionage_success')->after('stat_defending_success')->default(0);
+            $table->unsignedInteger('stat_spell_success')->after('stat_espionage_success')->default(0);
+            $table->unsignedInteger('stat_total_platinum_production')->after('stat_spell_success')->default(0);
+            $table->unsignedInteger('stat_total_food_production')->after('stat_total_platinum_production')->default(0);
+            $table->unsignedInteger('stat_total_lumber_production')->after('stat_total_food_production')->default(0);
+            $table->unsignedInteger('stat_total_mana_production')->after('stat_total_lumber_production')->default(0);
+            $table->unsignedInteger('stat_total_ore_production')->after('stat_total_mana_production')->default(0);
+            $table->unsignedInteger('stat_total_gem_production')->after('stat_total_ore_production')->default(0);
+            $table->unsignedInteger('stat_total_tech_production')->after('stat_total_gem_production')->default(0);
+            $table->unsignedInteger('stat_total_boat_production')->after('stat_total_tech_production')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('dominions', static function (Blueprint $table) {
+            $table->dropColumn('stat_attacking_success');
+            $table->dropColumn('stat_defending_success');
+            $table->dropColumn('stat_espionage_success');
+            $table->dropColumn('stat_spell_success');
+            $table->dropColumn('stat_total_platinum_production');
+            $table->dropColumn('stat_total_food_production');
+            $table->dropColumn('stat_total_lumber_production');
+            $table->dropColumn('stat_total_mana_production');
+            $table->dropColumn('stat_total_ore_production');
+            $table->dropColumn('stat_total_gem_production');
+            $table->dropColumn('stat_total_tech_production');
+            $table->dropColumn('stat_total_boat_production');
+        });
+    }
+}
