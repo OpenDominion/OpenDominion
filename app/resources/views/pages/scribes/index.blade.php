d73a5b25404d6be79810ff71926f129048f0c00d
WaveHack <email@wavehack.net>
2019-08-08 23:09:38 +0200

    Restyle scribes index page a bit

--- app/resources/views/pages/scribes/index.blade.php
+++ app/resources/views/pages/scribes/index.blade.php
@@ -5,38 +5,40 @@
         <div class="box-header with-border">
             <h3 class="box-title">Races</h3>
         </div>
-        <div class="box-body table-responsive no-padding">
-            <div class="col-md-12 col-md-6">
-                <div class="box-header with-border">
-                    <h4 class="box-title">Good</h4>
+        <div class="box-body no-padding">
+            <div class="row">
+                <div class="col-md-12 col-md-6">
+                    <div class="box-header with-border">
+                        <h4 class="box-title">Good Alignment</h4>
+                    </div>
+                    <table class="table table-striped" style="margin-bottom: 0">
+                        <tbody>
+                            @foreach ($goodRaces as $race)
+                                <tr>
+                                    <td>
+                                        <a href="{{ route('scribes.race', str_slug($race['name'])) }}">{{ $race['name'] }}</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-                <table class="table table-striped">
-                    <tbody>
-                        @foreach ($goodRaces as $race)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('scribes.race', str_slug($race['name'])) }}">{{ $race['name'] }}</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-md-12 col-md-6">
-                <div class="box-header with-border">
-                    <h4 class="box-title">Evil</h4>
+                <div class="col-md-12 col-md-6">
+                    <div class="box-header with-border">
+                        <h4 class="box-title">Evil Alignment</h4>
+                    </div>
+                    <table class="table table-striped" style="margin-bottom: 0">
+                        <tbody>
+                            @foreach ($evilRaces as $race)
+                                <tr>
+                                    <td>
+                                        <a href="{{ route('scribes.race', $race['name']) }}">{{ $race['name'] }}</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-                <table class="table table-striped">
-                    <tbody>
-                        @foreach ($evilRaces as $race)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('scribes.race', $race['name']) }}">{{ $race['name'] }}</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
