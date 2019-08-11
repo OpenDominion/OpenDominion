dd53960eca8eabd2a419a4a96b36b49eff681d93
WaveHack <email@wavehack.net>
2019-08-11 22:51:47 +0200

    Remove dropIndex on foreign keys from migration

--- app/database/migrations/2019_06_24_192144_remove_info_ops_source_realm_target_dominion_constraint.php
+++ app/database/migrations/2019_06_24_192144_remove_info_ops_source_realm_target_dominion_constraint.php
@@ -18,8 +18,6 @@ class RemoveInfoOpsSourceRealmTargetDominionConstraint extends Migration
                 ->after('data')
                 ->default(true);
 
-            $table->index(['source_realm_id']);
-            $table->index(['target_dominion_id']);
             $table->dropUnique(['source_realm_id', 'target_dominion_id', 'type']);
         });
     }
@@ -32,11 +30,9 @@ class RemoveInfoOpsSourceRealmTargetDominionConstraint extends Migration
     public function down(): void
     {
         Schema::table('info_ops', static function (Blueprint $table) {
-            $table->dropColumn(['latest']);
-
             $table->unique(['source_realm_id', 'target_dominion_id', 'type']);
-            $table->dropIndex(['source_realm_id']);
-            $table->dropIndex(['target_dominion_id']);
+
+            $table->dropColumn(['latest']);
         });
     }
 }
