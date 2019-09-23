515e9a9de4b9ec24273073e4218a06c2cd4c3fd3
WaveHack <email@wavehack.net>
2019-09-23 20:38:48 +0200

    Fix copying laradock override configuration files

--- bin/01-start.sh
+++ bin/01-start.sh
@@ -2,7 +2,7 @@
 
 # Copy the Laradock configuration files
 cp .laradock/.env laradock/
-cp -R .laradock/ laradock/
+cp -R .laradock/** laradock/
 
 # Build and start the Laradock Docker containers
 cd laradock && docker-compose up --build -d workspace php-fpm nginx mariadb
