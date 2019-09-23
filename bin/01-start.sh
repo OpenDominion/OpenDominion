cf84e7282c66be3fff166171548d4d467fe726ab
WaveHack <email@wavehack.net>
2019-09-23 19:25:13 +0200

    Store Laradock config (override) files in /.laradock

--- bin/01-start.sh
+++ bin/01-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-# Copy the Laradock configuration file
-cp .env.laradock laradock/.env
+# Copy the Laradock configuration files
+cp -R .laradock laradock
 
 # Build and start the Laradock Docker containers
 cd laradock && docker-compose up --build -d workspace php-fpm nginx mariadb
