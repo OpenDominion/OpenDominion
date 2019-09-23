8b5cbb0f09c852fda66092c2cbdb3286ef563714
WaveHack <email@wavehack.net>
2019-09-23 19:27:15 +0200

    Update start script

--- bin/01-start.sh
+++ bin/01-start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Copy the Laradock configuration files
-cp -R .laradock laradock
+cp .laradock/.env laradock/
+cp -R .laradock/ laradock/
 
 # Build and start the Laradock Docker containers
 cd laradock && docker-compose up --build -d workspace php-fpm nginx mariadb
