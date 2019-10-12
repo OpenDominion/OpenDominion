41cbb6cba6253591f51ea6aed6820cc115baf46e
John Strömblom <john.stromblom@outlook.com>
2019-10-12 14:17:21 +0200

    #730 Buildings added to scribes

--- src/Http/Controllers/ScribesController.php
+++ src/Http/Controllers/ScribesController.php
@@ -2,6 +2,7 @@
 
 namespace OpenDominion\Http\Controllers;
 
+use OpenDominion\Helpers\BuildingHelper;
 use OpenDominion\Helpers\LandHelper;
 use OpenDominion\Helpers\RaceHelper;
 use OpenDominion\Helpers\SpellHelper;
@@ -12,10 +13,27 @@ class ScribesController extends AbstractController
 {
     public function getIndex()
     {
+        $buildingHelper = app(BuildingHelper::class);
+
+        $buildingTypesPerLandType = $buildingHelper->getBuildingTypesByRace();
+        $buildingTypeWithLandType = [];
+        foreach ($buildingTypesPerLandType as $landType => $buildingTypes) {
+            foreach($buildingTypes as $buildingType) {
+                $buildingTypeWithLandType[$buildingType] = $landType;
+            }
+        }
+
+        $buildingTypeWithLandType['home'] = null;
+
+        ksort($buildingTypeWithLandType);
+
         $races = collect(Race::orderBy('name')->get())->groupBy('alignment')->toArray();
         return view('pages.scribes.index', [
             'goodRaces' => $races['good'],
             'evilRaces' => $races['evil'],
+            'buildingTypeWithLandType' => $buildingTypeWithLandType,
+            'buildingHelper' => $buildingHelper,
+            'landHelper' => app(LandHelper::class),
         ]);
     }
 
