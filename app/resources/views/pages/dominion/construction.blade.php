cd2ee442410df836f20ed65259a2db73b944adcc
internetfett <internetfett@gmail.com>
2020-02-25 10:44:30 -0600

    show discounted land costs

--- app/resources/views/pages/dominion/construction.blade.php
+++ app/resources/views/pages/dominion/construction.blade.php
@@ -59,7 +59,15 @@
                     <p>Construction will let you construct additional buildings and will take <b>12 hours</b> to process.</p>
                     <p>Construction per building will come at a cost of 1 acre of barren land of the building type, {{ number_format($constructionCalculator->getPlatinumCost($selectedDominion)) }} platinum and {{ number_format($constructionCalculator->getLumberCost($selectedDominion)) }} lumber.</p>
                     <p>You have {{ number_format($landCalculator->getTotalBarrenLand($selectedDominion)) }} {{ str_plural('acre', $landCalculator->getTotalBarrenLand($selectedDominion)) }} of barren land, {{ number_format($selectedDominion->resource_platinum) }} platinum and {{ number_format($selectedDominion->resource_lumber) }} lumber.</p>
-                    @if ($selectedDominion->discounted_land)<p>Additionally, {{ $selectedDominion->discounted_land }} acres from invasion can be built at reduced cost.</p>@endif
+                    @if ($selectedDominion->discounted_land)
+                        <p>Additionally, {{ $selectedDominion->discounted_land }} acres from invasion can be built at reduced cost.</p>
+                        @if ($selectedDominion->discounted_land > $landCalculator->getTotalBarrenLand($selectedDominion))
+                            <p>The first {{ $selectedDominion->discounted_land - $landCalculator->getTotalBarrenLand($selectedDominion) }} acres you construct will cost {{ number_format($constructionCalculator->getPlatinumCost($selectedDominion) / 4) }} platinum and {{ number_format($constructionCalculator->getLumberCost($selectedDominion) / 4) }} lumber.</p>
+                            <p>The remaining acres will cost {{ number_format($constructionCalculator->getPlatinumCost($selectedDominion) / 2) }} platinum and {{ number_format($constructionCalculator->getLumberCost($selectedDominion) / 2) }} lumber.</p>
+                        @else
+                            <p>The discounted acres will come at cost of {{ number_format($constructionCalculator->getPlatinumCost($selectedDominion) / 2) }} platinum and {{ number_format($constructionCalculator->getLumberCost($selectedDominion) / 2) }} lumber.</p>
+                        @endif
+                    @endif
                     <p>You can afford to construct <b>{{ number_format($constructionCalculator->getMaxAfford($selectedDominion)) }} {{ str_plural('building', $constructionCalculator->getMaxAfford($selectedDominion)) }}</b> at that rate.</p>
                     <p>You may also <a href="{{ route('dominion.destroy') }}">destroy buildings</a> if you wish.</p>
                 </div>
