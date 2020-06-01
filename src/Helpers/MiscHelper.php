8e8956d7df1224d9ed137357451ded6e46e9ad0a
John Strömblom <john.stromblom@outlook.com>
2020-06-01 20:49:08 +0200

    rewording

--- src/Helpers/MiscHelper.php
+++ src/Helpers/MiscHelper.php
@@ -11,7 +11,7 @@ class MiscHelper
             'lumber' => 'Produced via lumberyards.<br>Used for constructing buildings.',
             'mana' => 'Produced via towers.<br>Used for casting spells.',
             'ore' => 'Produced via ore mines.<br>Used to train <i>some</i> units.',
-            'gems' => 'Produced via diamond mines.<br>Only used for investing.',
+            'gems' => 'Produced via diamond mines.<br>Only used for improvements.',
             'tech' => 'Produced via schools or invasions.<br>Used to gain techs.',
             'boats' => 'Produced via docks.<br>Used by <i>most</i> races during invasions.<br>Each boat carries 30 units (40 for Kobold).',
         ];
