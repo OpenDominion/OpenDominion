f3321f3704908d6a967c17f8a8b6e292f4f0b590
John Strömblom <john.stromblom@outlook.com>
2020-06-09 23:17:13 +0200

    Added more filtering and info to game event view

--- app/resources/views/pages/staff/moderator/dominions/showGameEvent.blade.php
+++ app/resources/views/pages/staff/moderator/dominions/showGameEvent.blade.php
@@ -5,7 +5,16 @@
 @section('content')
     <span>{{ $gameEvent->created_at }}</span>
     {{ $gameEvent->target->name }} (#{{ $gameEvent->target->realm->number }})
-
+    <select id="realm-select" class="form-control pull-right">
+        <option value="">
+            All
+        </option>
+        @foreach ($realmNumbers as $realmNumber)
+            <option value="{{ $realmNumber }}" {{ $realmNumber == $selectedRealmNumber ? 'selected' : null }}>
+                #{{ $realmNumber }}
+            </option>
+        @endforeach
+    </select>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -14,6 +23,9 @@
                 </th>
                 <th>
                     Type
+                </th>
+                <th>
+                    Performed by
                 </th>
             </tr>
         </thead>
@@ -27,7 +39,7 @@
                     $markedOlderOps = true;
                 @endphp
                     <tr>
-                        <td colspan="2" class="text-center">
+                        <td colspan="3" class="text-center">
                             Ops taken 24 hours or more before event
                         </td>
                     </tr>
@@ -39,9 +51,39 @@
                     <td>
                         {{ $infoOp->type }}
                     </td>
+                    <td>
+                        {{ $infoOp->sourceDominion->name }} (#{{ $infoOp->sourceDominion->realm->number }})
+                    </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
 
 @endsection
+
+@push('page-styles')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/select2/css/select2.min.css') }}">
+@endpush
+
+@push('page-scripts')
+    <script type="text/javascript" src="{{ asset('assets/vendor/select2/js/select2.full.min.js') }}"></script>
+@endpush
+
+@push('inline-scripts')
+    <script type="text/javascript">
+        (function ($) {
+            $('#realm-select').select2({ width: '225px' }).change(function() {
+                var selectedRound = $(this).val();
+
+                if(selectedRound) {
+                    window.location.href = "{!! route('staff.moderator.dominion.event', [$dominion->id, $gameEvent->id]) !!}/?realm=" + selectedRound;
+                }
+                else {
+                    window.location.href = "{!! route('staff.moderator.dominion.event', [$dominion->id, $gameEvent->id]) !!}";
+                }
+            });
+
+            $('#realm-select + .select2-container').addClass('pull-right');
+        })(jQuery);
+    </script>
+@endpush
--- src/Http/Controllers/Staff/Moderator/DominionController.php
+++ src/Http/Controllers/Staff/Moderator/DominionController.php
@@ -10,6 +10,7 @@ use OpenDominion\Calculators\NetworthCalculator;
 use OpenDominion\Http\Controllers\AbstractController;
 use OpenDominion\Models\Dominion;
 use OpenDominion\Models\GameEvent;
+use OpenDominion\Models\InfoOp;
 use OpenDominion\Models\Round;
 use OpenDominion\Models\UserActivity;
 use OpenDominion\Services\GameEventService;
@@ -66,17 +67,27 @@ class DominionController extends AbstractController
         ]);
     }
 
-    public function showGameEvent(Dominion $dominion, GameEvent $gameEvent)
+    public function showGameEvent(Dominion $dominion, GameEvent $gameEvent, Request $request)
     {
         $timeOfEvent = Carbon::parse($gameEvent->created_at);
-        $infoOps = $dominion->realm->infoOps()
-            ->where('target_dominion_id', '=', $gameEvent->target_id)
+        $infoOps = InfoOp::where('target_dominion_id', '=', $gameEvent->target_id)
             ->where('created_at', '<', $timeOfEvent)
             ->orderBy('created_at', 'desc')
             ->get();
 
+        $realmNumbers = $infoOps->map(function ($infoOp) { return $infoOp->sourceDominion->realm->number;})->unique()->sort();
+
+        $selectedRealmNumber = $request->input('realm');
+        if($selectedRealmNumber) {
+            $infoOps = $infoOps->filter(function ($infoOp) use ($selectedRealmNumber) {
+                return $infoOp->sourceDominion->realm->number == $selectedRealmNumber;
+            });
+        }
+
         $lastDay = Carbon::parse($gameEvent->created_at)->subDay();
         return view('pages.staff.moderator.dominions.showGameEvent', [
+            'realmNumbers' => $realmNumbers,
+            'selectedRealmNumber' => $selectedRealmNumber ?? -1,
             'dominion' => $dominion,
             'gameEvent' => $gameEvent,
             'infoOps' => $infoOps,
