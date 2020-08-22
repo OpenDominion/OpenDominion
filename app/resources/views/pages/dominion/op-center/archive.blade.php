da507865567b45b3a9020d4ac358e9782164ba55
John Strömblom <john.stromblom@outlook.com>
2020-08-22 15:28:48 +0200

    New partials for improvements

--- app/resources/views/pages/dominion/op-center/archive.blade.php
+++ app/resources/views/pages/dominion/op-center/archive.blade.php
@@ -155,38 +155,7 @@
 
                         @slot('noPadding', true)
 
-                        <table class="table">
-                            <colgroup>
-                                <col width="150">
-                                <col>
-                                <col width="100">
-                            </colgroup>
-                            <thead>
-                                <tr>
-                                    <td>Part</td>
-                                    <td>Rating</td>
-                                    <td class="text-center">Invested</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($improvementHelper->getImprovementTypes() as $improvementType)
-                                    <tr>
-                                        <td>
-                                            {{ ucfirst($improvementType) }}
-                                            <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="{{ $improvementHelper->getImprovementHelpString($improvementType) }}"></i>
-                                        </td>
-                                        <td>
-                                            {{ sprintf(
-                                                $improvementHelper->getImprovementRatingString($improvementType),
-                                                number_format((array_get($infoOp->data, "{$improvementType}.rating") * 100), 2),
-                                                number_format((array_get($infoOp->data, "{$improvementType}.rating") * 100 * 2), 2)
-                                            ) }}
-                                        </td>
-                                        <td class="text-center">{{ number_format(array_get($infoOp->data, "{$improvementType}.points")) }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        @include('partials.dominion.improvements-table', ['data' => $infoOp->data])
 
                         @slot('boxFooter')
                             @if ($infoOp !== null)
