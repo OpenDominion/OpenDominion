8a6430ec1d8333a53b4d50493197bf6d02229a23
internetfett <internetfett@gmail.com>
2020-10-05 13:23:41 -0500

    reduce size of pagination buttons

--- app/resources/views/vendor/pagination/default.blade.php
+++ app/resources/views/vendor/pagination/default.blade.php
@@ -1,5 +1,5 @@
 @if ($paginator->hasPages())
-    <ul class="pagination no-margin" role="navigation">
+    <ul class="pagination pagination-sm no-margin" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
--- app/resources/views/vendor/pagination/simple-default.blade.php
+++ app/resources/views/vendor/pagination/simple-default.blade.php
@@ -1,5 +1,5 @@
 @if ($paginator->hasPages())
-    <ul class="pagination no-margin" role="navigation">
+    <ul class="pagination pagination-sm no-margin" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
