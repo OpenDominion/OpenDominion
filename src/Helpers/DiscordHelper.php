074829fab03dfa8984d845d0ea160508d420938f
John Strömblom <john.stromblom@outlook.com>
2020-10-15 23:11:50 +0200

    Fixed some configs

--- src/Helpers/DiscordHelper.php
+++ src/Helpers/DiscordHelper.php
@@ -9,12 +9,11 @@ class DiscordHelper
     public function getDiscordConnectUrl(): string
     {
         $clientId = config('app.discord_client_id');
-        return 'https://discord.com/api/oauth2/authorize?client_id=' . $clientId . '&redirect_uri=' . $this->getDiscordCallbackUrl() . '&response_type=code&scope=email%20identify';
+        return 'https://discord.com/api/oauth2/authorize?client_id=' . $clientId . '&redirect_uri=' . urlencode($this->getDiscordCallbackUrl()) . '&response_type=code&scope=email%20identify';
     }
 
     public function getDiscordCallbackUrl(): string
     {
-        $baseUrl = urlencode(request()->getSchemeAndHttpHost()."/");
-        return $baseUrl . 'discordCallback';
+        return request()->getSchemeAndHttpHost() . '/discordCallback';
     }
 }
