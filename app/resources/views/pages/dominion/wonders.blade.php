7bd00cbec2695de09a30f5e7dae5a4824071b97e
internetfett <internetfett@gmail.com>
2020-11-25 10:08:34 -0600

    fix wonder casualty info pane

--- app/resources/views/pages/dominion/wonders.blade.php
+++ app/resources/views/pages/dominion/wonders.blade.php
@@ -328,7 +328,7 @@
                 <div class="box-body">
                     <p>Wonders provide bonuses to all dominions in the controlling realm and are acquired by destroying and rebuilding them.</p>
                     <p>The first wave of wonders will appear on the 6th day of the round with a starting power of 150,000. An additional wonder will appear every 48 hours with a starting power of 250,000. Once rebuilt, wonder power depends on the damage your realm did to it and time into the round.</p>
-                    <p> When attacking wonders, your offense is <b>unmodded</b> (except by morale) and always suffers <b>5% casualties</b> (including immortal units). Each dominion that participates in destroying a wonder that is controlled by another realm is awarded prestige.</p>
+                    <p> When attacking wonders, your offense is <b>unmodded</b> (except by morale) and always suffers <b>3.5% casualties</b> (including immortal units). Each dominion that participates in destroying a wonder that is controlled by another realm is awarded prestige.</p>
                     @if ($selectedDominion->morale < 100)
                         <p>You have {{ $selectedDominion->morale }}% morale, which is reducing your offense and defense by {{ number_format(100 - $militaryCalculator->getMoraleMultiplier($selectedDominion) * 100, 2) }}%.</p>
                     @else
