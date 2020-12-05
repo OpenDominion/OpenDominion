4862e66ac1c297c7fa4862ebd35ffb1ac7e4536d
John Strömblom <john.stromblom@outlook.com>
2020-12-05 17:24:15 +0100

    Multiplier missed for urg smash

--- src/Services/Dominion/Actions/DestroyActionService.php
+++ src/Services/Dominion/Actions/DestroyActionService.php
@@ -55,11 +55,14 @@ class DestroyActionService
             $constructionCalculator = app(ConstructionCalculator::class);
             $multiplier =  $totalBuildingsToDestroy * $dominion->getTechPerkMultiplier('destruction_refund');
 
-            $platinumRefund = round($constructionCalculator->getPlatinumCostRaw($dominion) * $multiplier);
-            $lumberRefund = round($constructionCalculator->getLumberCostRaw($dominion) * $multiplier);
+            $platinumRefund = round($constructionCalculator->getPlatinumCostRaw($dominion));
+            $lumberRefund = round($constructionCalculator->getLumberCostRaw($dominion));
 
             $platinumRefund = min($platinumRefund, $constructionCalculator->getPlatinumCost($dominion));
             $lumberRefund = min($lumberRefund, $constructionCalculator->getLumberCost($dominion));
+
+            $platinumRefund *= $multiplier;
+            $lumberRefund *= $multiplier;
 
             $destructionRefundString = " You were refunded {$platinumRefund} platinum and {$lumberRefund} lumber.";
             $dominion->resource_platinum += $platinumRefund;
