6aa3dab8699a6300e607af2d257079ff02c46de7
internetfett <internetfett@gmail.com>
2021-02-06 10:40:22 -0600

    wonders not visible in one-way war

--- app/resources/views/pages/dominion/wonders.blade.php
+++ app/resources/views/pages/dominion/wonders.blade.php
@@ -90,7 +90,7 @@
                                     <select name="target_wonder" id="target_wonder" class="form-control select2" required style="width: 100%" data-placeholder="Select a target wonder" {{ $selectedDominion->isLocked() ? 'disabled' : null }}>
                                         <option></option>
                                         @foreach ($wonders as $wonder)
-                                            @if ($wonder->realm == null || $governmentService->isWarEscalated($selectedDominion->realm, $wonder->realm))
+                                            @if ($wonder->realm == null || $governmentService->isWarEscalated($selectedDominion->realm, $wonder->realm) || $governmentService->isWarEscalated($wonder->realm, $selectedDominion->realm))
                                                 <option value="{{ $wonder->id }}" data-war="{{ $wonder->realm !== null ? 1 : 0 }}">
                                                     {{ $wonder->wonder->name }}
                                                     @if ($wonder->realm !== null)
