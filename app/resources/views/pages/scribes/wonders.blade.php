780a354a7379fd57b848650f0b55d721e6779c3e
internetfett <internetfett@gmail.com>
2021-03-14 11:15:37 -0500

    update casualties on wonders scribes page

--- app/resources/views/pages/scribes/wonders.blade.php
+++ app/resources/views/pages/scribes/wonders.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-md-12">
                     <p>Wonders provide bonuses to all dominions in the controlling realm and are acquired by destroying and rebuilding them.</p>
                     <p>The first wave of wonders will appear on the 6th day of the round with a starting power of 150,000. An additional wonder will appear every 48 hours with a starting power of 250,000. Once rebuilt, wonder power depends on the damage your realm did to it and time into the round.</p>
-                    <p> When attacking wonders, your offense is <b>unmodded</b> (except by morale) and always suffers <b>5% casualties</b> (including immortal units). Each dominion that participates in destroying a wonder that is controlled by another realm is awarded prestige.</p>
+                    <p> When attacking wonders, your offense is <b>unmodded</b> (except by morale) and always suffers <b>3.5% casualties</b> (including immortal units). Each dominion that participates in destroying a wonder that is controlled by another realm is awarded prestige.</p>
                     <em>
                         <p>More information can be found on the <a href="https://wiki.opendominion.net/wiki/Wonders">wiki</a>.</p>
                     </em>
