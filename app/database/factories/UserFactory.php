fd5d8c1ee0276ef4f5a0f84428f9f0393f6f963f
internetfett <internetfett@gmail.com>
2021-04-14 11:49:02 -0500

    fix imports for laravel 8

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -1,18 +1,35 @@
 <?php
 
-use Faker\Generator as Faker;
+namespace Database\Factories;
 
-/** @var Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(\OpenDominion\Models\User::class, function (Faker $faker) {
-    static $password;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use OpenDominion\Models\User;
 
-    return [
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'display_name' => $faker->unique()->name,
-        'remember_token' => str_random(10),
-        'activated' => true,
-        'activation_code' => str_random(),
-        'last_online' => null,
-    ];
-});
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('secret'),
+            'display_name' => $this->faker->unique()->name,
+            'remember_token' => Str::random(10),
+            'activated' => true,
+            'activation_code' => Str::random(),
+            'last_online' => null,
+        ];
+    }
+}
