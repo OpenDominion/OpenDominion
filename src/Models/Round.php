3415dd7dbd00ada78ca73b95f8675315058e6718
internetfett <internetfett@gmail.com>
2022-01-17 20:06:58 -0600

    fix realm assignment time

--- src/Models/Round.php
+++ src/Models/Round.php
@@ -154,8 +154,8 @@ class Round extends AbstractModel
         $assignmentHours = \OpenDominion\Services\RealmFinderService::ASSIGNMENT_HOURS_BEFORE_START;
 
         return $query
-            ->where('start_date', '<', now()->subHours($assignmentHours))
-            ->where('start_date', '>=', now()->subHours($assignmentHours + 1));
+            ->where('start_date', '<', now()->addHours($assignmentHours))
+            ->where('start_date', '>=', now()->addHours($assignmentHours - 1));
     }
 
     /**
