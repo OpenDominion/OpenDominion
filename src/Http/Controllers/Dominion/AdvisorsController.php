760cf0474b54c2d57a9cdbf89ada45ccfbba540f
internetfett <internetfett@gmail.com>
2022-02-03 19:42:17 -0600

    make advisors logic consistent

--- src/Http/Controllers/Dominion/AdvisorsController.php
+++ src/Http/Controllers/Dominion/AdvisorsController.php
@@ -229,32 +229,42 @@ class AdvisorsController extends AbstractDominionController
 
         $dominion = $this->getSelectedDominion();
 
-        if ($dominion->locked_at !== null) {
-            throw new GameException('Locked dominions are not allowed to look at realm advisors.');
+        if ($dominion->id == $target->id) {
+            return;
         }
 
         if ($dominion->realm_id !== $target->realm_id) {
             throw new GameException('You are only allowed to look at dominions in your realm.');
         }
 
-        if ($dominion->id == $target->id) {
-            return;
+        if ($dominion->locked_at !== null) {
+            throw new GameException('Locked dominions are not allowed to look at realm advisors.');
         }
 
-        $realmAdvisors = $target->getSetting('realmadvisors');
+        $dominionAdvisors = $target->getSetting('realmadvisors');
 
         // Realm Advisor is explicitly enabled
-        if ($realmAdvisors && array_key_exists($dominion->id, $realmAdvisors) && $realmAdvisors[$dominion->id] === true) {
+        if ($dominionAdvisors && array_key_exists($dominion->id, $dominionAdvisors) && $dominionAdvisors[$dominion->id] === true) {
             return;
         }
 
         // Realm Advisor is explicity disabled
-        if ($realmAdvisors && array_key_exists($dominion->id, $realmAdvisors) && $realmAdvisors[$dominion->id] === false) {
+        if ($dominionAdvisors && array_key_exists($dominion->id, $dominionAdvisors) && $dominionAdvisors[$dominion->id] === false) {
             throw new GameException('This user has opted not to share their advisors.');
         }
 
         // Pack Advisor is enabled
         if ($target->user != null && $target->user->getSetting('packadvisors') !== false && ($dominion->pack_id != null && $dominion->pack_id == $target->pack_id)) {
+            return;
+        }
+
+        // Late starters disabled by default
+        if ($dominion->created_at > $dominion->round->realmAssignmentDate()) {
+            throw new GameException('This user has opted not to share their advisors.');
+        }
+
+        // Realm Advisor is enabled
+        if ($target->user !== null && $target->user->getSetting('realmadvisors') !== false) {
             return;
         }
 
