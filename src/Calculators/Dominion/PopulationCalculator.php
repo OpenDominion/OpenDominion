4bea62f800b7a5afbb65f7648d58ce58c9c29077
internetfett <internetfett@gmail.com>
2024-04-01 16:18:34 -0500

    fix burning pop growth

--- src/Calculators/Dominion/PopulationCalculator.php
+++ src/Calculators/Dominion/PopulationCalculator.php
@@ -275,7 +275,7 @@ class PopulationCalculator
             $peasantsProtected = $opsCalculator->getPeasantsProtected($dominion);
             $maxPeasants = $this->getMaxPeasantPopulation($dominion);
             if ($dominion->peasants >= $peasantsProtected) {
-                $peasantBirth = round($peasantsVulnerable * $fixedGrowth) + round($maxPeasants * $fixedGrowth / 10);
+                $peasantBirth = round($peasantsVulnerable * $fixedGrowth) + round($maxPeasants * 0.05);
             }
         }
 
