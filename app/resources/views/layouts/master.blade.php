8bc1fb586eb502b5d0ca6faa7e71284df200b1c7
internetfett <internetfett@gmail.com>
2024-09-29 09:59:07 -0500

    attempt to fix dark theme controls

--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -17,16 +17,16 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="apple-mobile-web-app-title" content="OpenDominion">
     <meta name="application-name" content="OpenDominion">
-    {% if (Auth::user() && Auth::user()->skin == 'skin-classic') %}
+    @if (Auth::user() && Auth::user()->skin == 'skin-classic')
         <meta name="theme-color" content="#005566">
         <style type="text/css">
             :root {
                 color-scheme: dark;
             }
         </style>
-    {% else %}
+    @else
         <meta name="theme-color" content="#ffffff">
-    {% endif %}
+    @endif
 
     @include('partials.styles')
 
