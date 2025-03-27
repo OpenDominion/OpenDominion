2c9f149f0900d6d626f939e9f4827610fd247b27
internetfett <internetfett@gmail.com>
2025-03-27 16:26:50 -0500

    remove actions from battle report

--- app/resources/views/pages/dominion/hero-battle-report.blade.php
+++ app/resources/views/pages/dominion/hero-battle-report.blade.php
@@ -47,79 +47,17 @@
                             </div>
                             <div class="row">
                                 <div class="col-sm-12">
-                                    @if ($battle->finished)
-                                        <div class="text-center">
+                                    <div class="text-center">
+                                        @if ($battle->finished)
                                             @if ($battle->winner == null)
                                                 <h4>Draw!</h4>
                                             @else
                                                 <h4>{{ $battle->winner->name }} wins!</h4>
                                             @endif
-                                        </div>
-                                    @else
-                                        <div class="form-group">
-                                            <div class="col-sm-6">
-                                                <label class="form-label">
-                                                    Actions in queue
-                                                </label>
-                                                <table class="table-condensed">
-                                                    @foreach ($playerCombatant->actions ?? [] as $idx => $action)
-                                                        <tr>
-                                                            <td>{{ $battle->current_turn + $idx }}</td>
-                                                            <td>{{ ucwords($action) }}</td>
-                                                            <td>
-                                                                <a href="{{ route('dominion.heroes.battles.action.delete', ['combatant'=>$playerCombatant->id, 'action'=>$idx]) }}">
-                                                                    <i class="fa fa-trash text-danger"></i>
-                                                                </a>
-                                                            </td>
-                                                        </tr>
-                                                    @endforeach
-                                                </table>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <label class="form-label">
-                                                    Perform/Queue an action
-                                                </label>
-                                                <div>
-                                                    @foreach ($heroHelper->getCombatActions() as $action)
-                                                        @if (!$heroHelper->canUseCombatAction($playerCombatant, $action) || $playerCombatant->time_bank <= 0)
-                                                            <a class="btn btn-block btn-default" disabled>
-                                                                {{ ucwords($action) }}
-                                                            </a>
-                                                        @else
-                                                            <a class="btn btn-block btn-primary" href="{{ route('dominion.heroes.battles.action', ['combatant'=>$playerCombatant->id, 'action'=>$action]) }}">
-                                                                {{ ucwords($action) }}
-                                                            </a>
-                                                        @endif
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <label class="form-label">
-                                                    Strategy <small>(for turns taken while offline)</small>
-                                                </label>
-                                                <select name="strategy" class="form-control">
-                                                    @foreach ($heroHelper->getCombatStrategies() as $strategy)
-                                                        <option value="{{ $strategy }}" {{ $strategy == $playerCombatant->strategy ? 'selected' : null }}>{{ ucwords($strategy) }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-9">
-                                                <div class="checkbox">
-                                                    <label>
-                                                        <input type="checkbox" name="automated" {{ $playerCombatant->automated != false ? 'checked' : null }}>
-                                                        Automate all of my turns
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-3">
-                                                <button class="btn btn-primary">Update</button>
-                                            </div>
-                                        </div>
-                                    @endif
+                                        @else
+                                            <h4>Combat in progress...</h4>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </div>
