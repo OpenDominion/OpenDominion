471286145dfd5e97c9d65739e1844e58325e1ce3
internetfett <internetfett@gmail.com>
2025-08-06 18:41:29 -0500

    fix importing quick start

--- src/Services/Dominion/AutomationService.php
+++ src/Services/Dominion/AutomationService.php
@@ -108,7 +108,7 @@ class AutomationService
                         });
                         continue;
                     }
-                    if ($dominion->protection_type == 'quick_start' && !(in_array($dominion->protection_ticks_remaining, [36, 24]) || $dominion->protection_ticks_remaining <= 12)) {
+                    if ($dominion->protection_type == 'quick' && !(in_array($dominion->protection_ticks_remaining, [36, 24]) || $dominion->protection_ticks_remaining <= 12)) {
                         // Don't process 'skipped' ticks for Quick Start
                         continue;
                     }
