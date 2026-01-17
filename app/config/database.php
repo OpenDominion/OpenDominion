c68756e2de8b2faef34f263b87506ab49e9002e7
internetfett <internetfett@gmail.com>
2026-01-17 11:14:14 -0600

    audit packages

--- app/config/database.php
+++ app/config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_PERSISTENT => true,
             ]) : [],
             'dump' => [
