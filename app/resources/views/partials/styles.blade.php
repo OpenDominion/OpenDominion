ace73148003e036d7ce0054d0ec33228979ebce0
internetfett <internetfett@gmail.com>
2026-03-07 17:35:15 -0600

    fix base font

--- app/resources/views/partials/styles.blade.php
+++ app/resources/views/partials/styles.blade.php
@@ -2,7 +2,9 @@
 @vite(['app/resources/sass/app.scss'])
 
 {{-- Google Fonts --}}
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600&display=swap">
 
 {{-- Page specific styles --}}
 @stack('page-styles')
