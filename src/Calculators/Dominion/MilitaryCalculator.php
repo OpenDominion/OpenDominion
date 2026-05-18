cef995ef438e87e535e67db1b8f2cfedad65e3fb
internetfett <internetfett@gmail.com>
2026-05-18 16:20:29 -0500

    fix busted date math

--- src/Calculators/Dominion/MilitaryCalculator.php
+++ src/Calculators/Dominion/MilitaryCalculator.php
@@ -1309,7 +1309,7 @@ class MilitaryCalculator
             return null;
         }
 
-        return (int) now()->endOfHour()->diffInHours($mostRecentInvasion);
+        return (int) now()->endOfHour()->diffInHours($mostRecentInvasion, true);
     }
 
     /**
