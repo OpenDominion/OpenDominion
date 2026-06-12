e3675a6beff88c6a48a934ac22b76c0cdbc846c4
internetfett <internetfett@gmail.com>
2026-06-11 23:10:12 -0500

    clean up spacing

--- app/resources/views/pages/dominion/automation/_action-form.blade.php
+++ app/resources/views/pages/dominion/automation/_action-form.blade.php
@@ -5,7 +5,7 @@
 <div class="action-form-container" id="{{ $formId }}">
     <div class="mb-2">
         Action:
-        <select class="form-select form-select-sm" name="action" {{ $isLocked ? 'disabled' : null }}>
+        <select class="form-select" name="action" {{ $isLocked ? 'disabled' : null }}>
             <option value="train" {{ $selectedAction == 'train' ? 'selected' : null }}>Train Military</option>
             <option value="construct" {{ $selectedAction == 'construct' ? 'selected' : null }}>Construct Buildings</option>
             <option value="explore" {{ $selectedAction == 'explore' ? 'selected' : null }}>Explore Land</option>
@@ -18,7 +18,7 @@
     </div>
     <div class="mb-2 action-options train" style="{{ $selectedAction != 'train' ? 'display: none;' : '' }}">
         Unit:
-        <select class="form-select form-select-sm" name="key" {{ ($selectedAction != 'train' || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key" {{ ($selectedAction != 'train' || $isLocked) ? 'disabled' : null }}>
             <option></option>
             @foreach ($unitTypes as $unitType)
                 <option value="{{ $unitType }}" {{ ($item && $selectedAction == 'train' && ($item['key'] ?? '') == $unitType) ? 'selected' : null }}>
@@ -29,7 +29,7 @@
     </div>
     <div class="mb-2 action-options construct" style="{{ $selectedAction != 'construct' ? 'display: none;' : '' }}">
         Building:
-        <select class="form-select form-select-sm" name="key" {{ ($selectedAction != 'construct' || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key" {{ ($selectedAction != 'construct' || $isLocked) ? 'disabled' : null }}>
             <option></option>
             @foreach ($buildings as $building)
                 <option value="{{ $building }}" {{ ($item && $selectedAction == 'construct' && ($item['key'] ?? '') == $building) ? 'selected' : null }}>
@@ -40,7 +40,7 @@
     </div>
     <div class="mb-2 action-options explore rezone" style="{{ !in_array($selectedAction, ['explore', 'rezone']) ? 'display: none;' : '' }}">
         Land Type:
-        <select class="form-select form-select-sm" name="key" {{ (!in_array($selectedAction, ['explore', 'rezone']) || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key" {{ (!in_array($selectedAction, ['explore', 'rezone']) || $isLocked) ? 'disabled' : null }}>
             <option></option>
             @foreach ($landTypes as $landType)
                 <option value="{{ $landType }}" {{ ($item && in_array($selectedAction, ['explore', 'rezone']) && ($item['key'] ?? '') == $landType) ? 'selected' : null }}>
@@ -51,7 +51,7 @@
     </div>
     <div class="mb-2 action-options rezone" style="{{ $selectedAction != 'rezone' ? 'display: none;' : '' }}">
         Target Land Type:
-        <select class="form-select form-select-sm" name="key2" {{ ($selectedAction != 'rezone' || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key2" {{ ($selectedAction != 'rezone' || $isLocked) ? 'disabled' : null }}>
             <option></option>
             @foreach ($landTypes as $landType)
                 <option value="{{ $landType }}" {{ ($item && $selectedAction == 'rezone' && ($item['key2'] ?? '') == $landType) ? 'selected' : null }}>
@@ -68,7 +68,7 @@
     </div>
     <div class="mb-2 action-options spell" style="{{ $selectedAction != 'spell' ? 'display: none;' : '' }}">
         Spell:
-        <select class="form-select form-select-sm" name="key" {{ ($selectedAction != 'spell' || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key" {{ ($selectedAction != 'spell' || $isLocked) ? 'disabled' : null }}>
             <option></option>
             @foreach ($spells as $spell)
                 <option value="{{ $spell->key }}" {{ ($item && $selectedAction == 'spell' && ($item['key'] ?? '') == $spell->key) ? 'selected' : null }}>
@@ -79,7 +79,7 @@
     </div>
     <div class="mb-2 action-options daily_bonus" style="{{ $selectedAction != 'daily_bonus' ? 'display: none;' : '' }}">
         Bonus:
-        <select class="form-select form-select-sm" name="key" {{ ($selectedAction != 'daily_bonus' || $isLocked) ? 'disabled' : null }}>
+        <select class="form-select" name="key" {{ ($selectedAction != 'daily_bonus' || $isLocked) ? 'disabled' : null }}>
             <option></option>
             <option value="land" {{ ($item && $selectedAction == 'daily_bonus' && ($item['key'] ?? '') == 'land') ? 'selected' : null }}>Land</option>
             <option value="platinum" {{ ($item && $selectedAction == 'daily_bonus' && ($item['key'] ?? '') == 'platinum') ? 'selected' : null }}>Platinum</option>
